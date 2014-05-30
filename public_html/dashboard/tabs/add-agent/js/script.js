01f035d766050a7dca9b23972b47d9f01f83f825
Bryan Kneis <bkneis>
2014-05-30 14:00:03 +0000

    Ajax added to js file

--- public_html/dashboard/tabs/add-agent/js/script.js
+++ public_html/dashboard/tabs/add-agent/js/script.js
@@ -1,3 +1,24 @@
 $(function(){
-	
+	$("body").delegate("#add-agent", "click", function(e){
+
+		e.preventDefault();
+		
+		$.ajax({
+			url: "/api/agent/add",
+			type: "POST",
+			dataType: "json",
+			data: $('form#add-agent-form').serialize(),
+			async: false,
+			success: function(data){
+				//successfull so reload this content and show success message
+				pageMssg("Agent Added, you can edit agents in the edit agent tab", true);
+				
+				window.location.hash = "";
+				$("#wrapper").html(LOADER);
+				
+				window.location.hash = "add-agent";
+				
+			}
+		});
+	});
 });
