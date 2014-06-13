5eb4f95d9764c4ead8f311bbc18171c410af2540
Soren Schwert <soren@scubawhere.com>
2014-06-13 13:22:05 +0100

    Fix doubled/trippled ... event handlers on Create Trip tab.

--- public_html/dashboard/tabs/create-trip/js/dive-locator.js
+++ public_html/dashboard/tabs/create-trip/js/dive-locator.js
@@ -11,16 +11,16 @@ $(function(){
 			$("[name='_token']").val(token);
 		}
 	});
-	
-	$("body").delegate("#create-trip", "click", function(e){
+
+	$("#trip-form").delegate("#create-trip", "click", function(e){
 		e.preventDefault();
-		
+
 		if(!validateTrip()){
 			console.log("error");
 		}
-		
-		
-		
+
+
+
 		$.ajax({
 			url: "/company/add-trip",
 			type: "POST",
@@ -30,39 +30,39 @@ $(function(){
 			success: function(data){
 				//successfull so reload this content and show success message
 				pageMssg("Trip created. You can now activate it in 'Activate Trip'.", true);
-				
+
 				//load in new content
 				//force it by unsetting and setting hash
 				window.location.hash = "";
 				$("#wrapper").html(LOADER);
-				
+
 				window.location.hash = "create-trip";
-				
+
 			}
 		});
 	});
-	
-	var spotSource = $("#selected-spot").html(); 
+
+	var spotSource = $("#selected-spot").html();
 	var spotTemplate = Handlebars.compile(spotSource);
-	
-	$("body").delegate(".add-location", "click", function(){
+
+	$("#trip-form").delegate(".add-location", "click", function(){
 		//append this to list of selected locations
-		
+
 		var thisLocation	 = $(this).attr("data-location").split(",");
 		var thisLocationName = thisLocation[0];
 		var thisLocationID 	 = thisLocation[1];
 		var thisLocationLat  = thisLocation[2];
 		var thisLocationLng  = thisLocation[3];
 		var spotData = {name: thisLocationName, id: thisLocationID, longitude: thisLocationLng, latitude: thisLocationLat};
-		
+
 		//if it isnt already selected
 		//$( "li" ).remove("#"+thisLocationLat);
 		$("#selected-spots").append(spotTemplate(spotData));
-		
-		
+
+
 	});
-	
-	$("body").delegate(".remove-location", "click", function(){
+
+	$("#trip-form").delegate(".remove-location", "click", function(){
 
 		var thisLocation	 = $(this).attr("data-location").split(",");
 		var thisLocationName = thisLocation[0];
@@ -77,10 +77,10 @@ $(function(){
 	});
 
 	$("#locations-list").html("");//refresh list
-	
-	var locSource = $("#locations").html(); 
+
+	var locSource = $("#locations").html();
 	var locTemplate = Handlebars.compile(locSource);
-	
+
 	$.ajax({
 				url: "/company/locations",
 				type: "GET",
@@ -89,13 +89,13 @@ $(function(){
 				async: false,
 				success: function(data){
 					$.each(data, function(){
-					
+
 						$("#locations-list").append(locTemplate(this));
-						
+
 						$("#hidden-spots").append("<input type='hidden' name='locations[]' value='" + this.id + "'>");
-						
+
 					});
-					
+
 				}
 			});
 
@@ -103,19 +103,19 @@ $(function(){
 
 function validateTrip(){
 	var bool = true;
-	
-	
+
+
 	for ( instance in CKEDITOR.instances ){
     CKEDITOR.instances[instance].updateElement();
 	}
-	
+
 	$("#cke_description").css("border-color", "");//reset the ckeditors border colo
-	
+
 	var validName 			= $("[name=name]").validateField(5, 100);
 	var validDuration 		= $("[name=duration]").validateNumericField(0, 999999);//no maximum
 	var validDescription 	= $("[name=description]").validateField(5, 3000);
 	if(!validDescription){$("#cke_description").css("border-color", "red");}//oesnt work with present validate script
-	
+
 	function validTripType(){
 		var validTT = true;
 		$("#type-error").html("");
@@ -123,7 +123,7 @@ function validateTrip(){
 			validPU = false;
 			$("#type-error").html("<div class='red-helper'>Please select at least one .</div>");
 		}
-		
+
 		return validTT;
 	}
 	validTripType();//complete actions
@@ -132,6 +132,6 @@ function validateTrip(){
 	}else{
 		pageMssg("There are errors.", false);
 	}
-	
+
 	return bool;
 }
