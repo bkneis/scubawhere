4cbe4e9fe35bb57db9866fed251d2f88417824c0
Andy Potts <andypotts92@gmail.com>
2014-08-23 13:12:13 +0100

    Added model

---
+++ app/models/Addons.php
@@ -0,0 +1,45 @@
+<?php
+
+use LaravelBook\Ardent\Ardent;
+use ScubaWhere\Helper;
+
+class Addons extends Ardent {
+	protected $guarded = array('id', 'created_at', 'updated_at');
+
+	public static $rules = array(
+		'name'            => 'required',
+		'description'     => '',
+		'price'     			=> 'required|numeric',
+		'compulsory'  		=> 'required'
+	);
+
+	public function beforeSave()
+	{
+		if( isset($this->name) )
+			$this->name = Helper::sanitiseString($this->name);
+
+		if( isset($this->description) )
+			$this->description = Helper::sanitiseString($this->description);
+
+		if( isset($this->price) )
+			$this->price = Helper::sanitiseString($this->price);
+
+		if( isset($this->compulsory) )
+			$this->compulsory = Helper::sanitiseString($this->compulsory);
+	}
+
+	public function bookings()
+	{
+		return $this->hasMany('Booking');
+	}
+
+	public function company()
+	{
+		return $this->belongsTo('Company', 'Customer');
+	}
+
+	public function customers()
+	{
+		return $this->hasManyThrough('Customer', 'Booking');
+	}
+}
