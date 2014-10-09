75a48d92e2bc540ebd6b9b01a4d330ef373acc48
unknown <Jon@.(none)>
2014-10-09 21:15:09 +0100

    Updated form to rename agent to addon. Also added currency field.

--- public_html/dashboard/tabs/add-ons/index.php
+++ public_html/dashboard/tabs/add-ons/index.php
@@ -2,24 +2,24 @@
 	<div class="row">
 		<div class="box30">
 			<label class="dgreyb">Available addons</label>
-			<div class="padder" id="agent-list-container">
+			<div class="padder" id="addon-list-container">
 				<!--<div class="yellow-helper">
-					Select an agent to change its details.
+					Select an addon to change its details.
 				</div>-->
-				<button id="change-to-add-agent" style="padding: 0.5em 1em; margin: 0.4em;" class="bttn greenb">&plus; Add Addon</button>
-				<script type="text/x-handlebars-template" id="agent-list-template">
-					<ul id="agent-list">
+				<button id="change-to-add-addon" style="padding: 0.5em 1em; margin: 0.4em;" class="bttn greenb">&plus; Add Addon</button>
+				<script type="text/x-handlebars-template" id="addon-list-template">
+					<ul id="addon-list">
 
 					</ul>
 				</script>
 			</div>
 		</div>
-		<div class="box70" id="agent-form-container">
+		<div class="box70" id="addon-form-container">
 
-			<script type="text/x-handlebars-template" id="agent-form-template">
+			<script type="text/x-handlebars-template" id="addon-form-template">
 				<label class="dgreyb">{{task}} addon</label>
 				<div class="padder">
-					<form id="{{task}}-agent-form">
+					<form id="{{task}}-addon-form">
 						<div class="form-row">
 							<label class="field-label">Addon Name</label>
 							<input type="text" name="name" value="{{{name}}}">
@@ -27,16 +27,19 @@
 
 						<div class="form-row">
 							<label class="field-label">Addon Description</label>
-							<textarea name="addon_address" rows="3" cols="10">{{{addon_address}}}</textarea>
+							<textarea name="description" rows="3" cols="10">{{{description}}}</textarea>
 						</div>
 
 						<div class="form-row">
-							<label class="field-label">Price</label>
-							<input type="text" name="addon_phone" placeholder="£0.00" value="{{{addon_phone}}}">
+							<label class="field-label">Addon Price</label>
+							<select name="currency">
+								<option>GBP</option>
+							</select>
+							<input type="text" name="price" placeholder="0.00" value="{{{price}}}">
 						</div>
 
 						<div class="form-row">
-							<label class="field-label">Compulsory</label>
+							<label class="field-label">Compulsory?</label>
 							<input type="checkbox" name="addon_compulsory" placeholder="" value="{{{addon_email}}}">
 						</div>
 
@@ -46,7 +49,7 @@
 						{{/if}}
 						<input type="hidden" name="_token">
 
-						<button class="bttn blueb big-bttn" id="{{task}}-agent">{{task}} Addon</button>
+						<button class="bttn blueb big-bttn" id="{{task}}-addon">{{task}} Addon</button>
 
 					</form>
 				</div>
