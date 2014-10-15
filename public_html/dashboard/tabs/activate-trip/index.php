64510fb301bd222d26cc6508fba583e04620e71a
Soren Schwert <soren@scubawhere.com>
2014-10-15 15:56:14 +0100

    Restrict iterations input field on timetables to numbers creater/equal to 0.

--- public_html/dashboard/tabs/activate-trip/index.php
+++ public_html/dashboard/tabs/activate-trip/index.php
@@ -100,7 +100,7 @@
 						</table>
 						<input type="hidden" name="_token">
 						<input type="hidden" name="session_id" value="{{session.id}}">
-						Iterations: <input type="number" name="iterations" value="0" style="width: 50px;"> (default: 1.5 years worth)
+						Iterations: <input type="number" name="iterations" value="0" min="0" style="width: 50px;"> (default: 1.5 years worth)
 						<button class="bttn big-bttn blueb create-timetable-button" style="float: right;">Create timetable</button>
 					</form>
 				{{/unless}}
