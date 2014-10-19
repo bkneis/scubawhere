87da5f9832ff6e36cb020bcaee3628bc58025064
Jon Hiden <j_hiden@hotmail.com>
2014-10-19 18:32:42 +0100

    Added migrations & initial assertions to base TestCase

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,6 +1,43 @@
 <?php
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
+	
+	
+	/**
+	 * Default preparation for each test
+	 */
+	public function setUp()
+	{
+		parent::setUp();
+	
+		//Migrate to the temporary db
+		Artisan::call('migrate');
+		
+		//@TODO seed tables
+	
+		$models = array(
+			'Accomodation', 'Addon', 'Agency', 'Agent',
+			'Boat', 'Booking', 'Bookingdetail',
+			'Certificate', 'Company', 'Continent', 'Country', 'Customer',
+			'Departure',
+			'Location',
+			'Package', 'Packagefacade', 'Payment', 'Paymentgateway'.
+			'Ticket', 'Timetable', 'Trip', 'Triptype'
+		);
+	
+		//Reset event listeners on all models
+		foreach ($models as $model){
+			//Flush any exiting listeners
+			call_user_func(array($model, 'flushEventListeners'));
+			//Reregister listeners
+			call_user_func(array($model, 'boot'));
+		}
+	
+		//Assert tables are empty
+		foreach ($models as $model){
+			$this->assertCount(0, call_user_func(array($model, 'all')), $model." table should be empty!");
+		}
+	}
 
 	/**
 	 * Creates the application.
