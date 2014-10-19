c07bd424738296d0ac52c8d2aa72f75b1247077b
Jon Hiden <j_hiden@hotmail.com>
2014-10-19 19:52:14 +0100

    Adjustment to TestCase to migrate db for each test class, not each test (too slow!)

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,44 +1,51 @@
 <?php
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
+
+	public static $databaseMigrated = false;	
 	
+	public static $testModels = array(
+		'Accommodation', 'Addon', 'Agency', 'Agent',
+		'Boat', 'Booking', 'Bookingdetail',
+		'Certificate', 'Company', 'Continent', 'Country', 'Customer',
+		'Departure',
+		'Location',
+		'Package', 'Packagefacade', 'Payment', /*'Paymentgateway',*/
+		'Ticket', 'Timetable', 'Trip', 'Triptype'
+	);
 	
 	/**
-	 * Default preparation for each test
+	 * Prepare for each test
 	 */
 	public function setUp()
 	{
 		parent::setUp();
-	
-		//Migrate to the temporary db
-		Artisan::call('migrate');
 		
-		//@TODO seed tables
-	
-		$models = array(
-			'Accomodation', 'Addon', 'Agency', 'Agent',
-			'Boat', 'Booking', 'Bookingdetail',
-			'Certificate', 'Company', 'Continent', 'Country', 'Customer',
-			'Departure',
-			'Location',
-			'Package', 'Packagefacade', 'Payment', 'Paymentgateway'.
-			'Ticket', 'Timetable', 'Trip', 'Triptype'
-		);
-	
+		//Check if we need to migrate the database		
+		if (!self::$databaseMigrated) {
+			//Migrate the database
+			Artisan::call('migrate');
+			
+			//@TODO seed tables
+			//Artisan::call('db::seed');
+			
+			//Assert tables are empty
+			foreach (self::$testModels as $model){
+				$this->assertCount(0, call_user_func(array($model, 'all')), $model." table should be empty!");
+			}
+			
+			self::$databaseMigrated = true;
+		}
+		
 		//Reset event listeners on all models
-		foreach ($models as $model){
+		foreach (self::$testModels as $model){
 			//Flush any exiting listeners
 			call_user_func(array($model, 'flushEventListeners'));
 			//Reregister listeners
 			call_user_func(array($model, 'boot'));
 		}
-	
-		//Assert tables are empty
-		foreach ($models as $model){
-			$this->assertCount(0, call_user_func(array($model, 'all')), $model." table should be empty!");
-		}
 	}
-
+	
 	/**
 	 * Creates the application.
 	 *
