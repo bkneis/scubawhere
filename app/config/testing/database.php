172e99c0aff4fe437b91759f2661b32d68dcaff6
Jon Hiden <j_hiden@hotmail.com>
2014-10-20 20:05:06 +0100

    Changed setting to use local mysql database instead of sqlite in memory

--- app/config/testing/database.php
+++ app/config/testing/database.php
@@ -1,12 +1,17 @@
 <?php
 
 return array(
-	'default' => 'sqlite',
+	'default' => 'mysqltest',
 	'connections' => array(
-		'sqlite' => array(
-			'driver'   => 'sqlite',
-			'database' => ':memory:',
-			'prefix'   => ''
+		'mysqltest' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'scubawhere',
+			'username'  => 'root',
+			'password'  => '',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
 		),
 	)
 );
