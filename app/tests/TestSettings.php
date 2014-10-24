e8fd394bc6eb6d6fedfc2f3d0d444c9110a4950b
Jon Hiden <j_hiden@hotmail.com>
2014-10-24 22:34:13 +0100

    Created settings class to make updating tests easier

---
+++ app/tests/TestSettings.php
@@ -0,0 +1,61 @@
+<?php
+
+class TestSettings {
+
+	/**
+	 * Array of all unseeded models with their table names
+	 * @var array of unseeded models (name => table)
+	 */
+	public static $modelTables = array(
+			'Accommodation' => 'accommodations',
+			'Addon' => 'addons',
+			'Agent' => 'agents',
+			'Boat' => 'boats',
+			'Booking' => 'bookings',
+			'Bookingdetail' => 'booking_details',
+			'Company' => 'companies',
+			'Customer' => 'customers',
+			'Departure' => 'sessions',
+			'Location' => 'locations',
+			'Package' => 'packages',
+			'Packagefacade' => 'packagefacades',
+			'Payment' => 'payments',
+			'Ticket' => 'tickets',
+			'Timetable' => 'timetables',
+			'Trip' => 'trips'
+	);
+	
+	
+	
+	/**
+	 * Array of all seeded models with their table names
+	 * @var array of seeded models (name => table)
+	*/
+	public static $seededModelTables = array(
+			'Agency' => 'agencies',
+			'Certificate' => 'certificates',
+			'Continent' => 'continents',
+			'Country' => 'countries',
+			'Paymentgateway' => 'paymentgateways',
+			'Triptype' => 'triptypes'
+	);
+	
+	
+	
+	/**
+	 * Array of all pivot table names
+	 * @var array of pivot table names
+	*/
+	public static $pivotTables = array(
+			'accommodation_boat',
+			'addon_bookingdetail',
+			'boat_ticket',
+			'company_location',
+			'location_trip',
+			'package_ticket',
+			'ticket_trip',
+			'trip_triptype'
+	);
+	
+}
+
