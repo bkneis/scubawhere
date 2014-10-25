12d505860e25cad0f88dfbbce2459f2f0a098694
Jon Hiden <j_hiden@hotmail.com>
2014-10-25 18:51:44 +0100

    Added controller for currencies. Completes issue #49.

---
+++ app/controllers/CurrencyController.php
@@ -0,0 +1,29 @@
+<?php
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+// use ScubaWhere\Helper;
+
+class CurrencyController extends Controller {
+
+	public function __construct()
+	{
+		$this->beforeFilter('csrf', array('on' => 'post'));
+	}
+
+	public function getIndex()
+	{
+		try
+		{
+			if( !Input::get('id') ) throw new ModelNotFoundException();
+			return Currency::findOrFail( Input::get('id') );
+		}
+		catch(ModelNotFoundException $e)
+		{
+			return Response::json( array('errors' => array('The currency could not be found.')), 404 ); // 404 Not Found
+		}
+	}
+
+	public function getAll()
+	{
+		return Currency::get();
+	}
+}
