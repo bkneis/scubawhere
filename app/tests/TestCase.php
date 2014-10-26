713e111c1bf500315b57322ab0c3ba14f387b4b7
Jon Hiden <j_hiden@hotmail.com>
2014-10-26 11:51:15 +0000

    Moved db methods from TestCase to new helper class. New functions added to help help speed up tests

--- app/tests/TestCase.php
+++ app/tests/TestCase.php
@@ -1,13 +1,14 @@
 <?php
 
+use Monolog\Handler\TestHandler;
 class TestCase extends Illuminate\Foundation\Testing\TestCase {
 
 	/**
 	 * Flag to check if at least one refresh has been ran for the <code>TestCase</code>
 	 * @var bool flag
 	 */
-	public static $databaseRefreshed = false;
-	
+	public static $dbInitialised = false;
+
 	
 	
 	/**
@@ -19,157 +20,17 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	{
 		parent::setUp();		
 		//Make sure at least one refresh is done
-		if (!self::$databaseRefreshed) {
-			echo("\nDB ".getenv('DATABASE_NAME').": Performing initial setup.....");
-			$this->dbRefresh(true);
-			self::$databaseRefreshed = true;			
+		if (!self::$dbInitialised) {
+			echo("\nDB ".getenv('DATABASE_NAME').": Initialising.....");
+			TestHelper::dbRefresh(true);
+			self::$dbInitialised = true;
+			echo("\nDB ".getenv('DATABASE_NAME').": Initialisation complete!");
 		}
-		$this->refreshListeners();
-	}
-	
-	
-	
-	/**
-	 * Helper function to call any needed migrations on the test database
-	 */
-	public function dbMigrate(){		
-		echo("\nDB ".getenv('DATABASE_NAME').": Migrating.....");
-		//Migrate the database
-		Artisan::call('migrate');
-	}
-	
-	
-	
-	/**
-	 * Helper function to clear all unseeded tables in the test database (including pivots)
-	 */
-	public function dbClear(){
-		echo("\nDB ".getenv('DATABASE_NAME').": Clearing tables.....");
-		$this->clearModelTables(TestSettings::$modelTables);
-		$this->clearTables(TestSettings::$pivotTables);
-	}
-	
-	
-	
-	/**
-	 * Helper function to clear and reseed all seeded tables in the test database
-	 */
-	public function dbSeed(){
-		echo("\nDB ".getenv('DATABASE_NAME').": Seeding.....");
-		//Turn foreign key checks off
-		DB::statement('SET FOREIGN_KEY_CHECKS=0;');// <- USE WITH CAUTION!
-		//Seed tables
-		Artisan::call('db:seed');
-		//Turn foreign key checks on
-		DB::statement('SET FOREIGN_KEY_CHECKS=1;');// <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
-		//Assert seeded tables have entries
-		foreach (TestSettings::$seededModelTables as $model => $table){
-			$this->assertNotCount(0, call_user_func(array($model, 'all')), $model." table should not be empty!");
-		}
-	}
-	
-	
-	
-	/**
-	 * Helper function to set up a 'clean' test database
-	 * It will do the following:
-	 * Reset all migrations (optional)
-	 * Perform any required migrations
-	 * Clear all unseeded & pivot tables
-	 * Reseed any seeded tables
-	 * @param bool $includeReset flag to set whether or not rollback all migrations first
-	 */
-	public function dbRefresh($includeReset = false){
-		echo("\nDB ".getenv('DATABASE_NAME').": Refreshing.....");
 		
-		//@TODO Fix issue with rollbacks before this can be used
-// 		if ($includeReset) {
-// 			echo("\nDB ".getenv('DATABASE_NAME').": Rolling back migrations.....");
-// 			//Turn foreign key checks off <- USE WITH CAUTION!
-// 			DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-// 			//Rollback all migrations			
-// 			Artisan::call('migrate:reset');
-// 			//Turn foreign key checks on <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
-// 			DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-// 		}
+		//TODO keep an eye on this - might be needed but it does slow things down
+		TestHelper::refreshListeners();
+	}
 
-		$this->dbMigrate();
-		$this->dbClear();
-		$this->dbSeed();
-		echo("\nDB ".getenv('DATABASE_NAME').": Refresh complete!\n");
-		self::$databaseRefreshed = true;
-	}
-	
-	
-	
-	/**
-	 * Private helper to quickly clear tables with models
-	 * This function will also assert that all of the tables are empty
-	 * @param array $modelTableArray to clear
-	 */
-	private function clearModelTables(array $modelTableArray){
-		
-		//Get count for each table & truncate it if its not empty
-		foreach ($modelTableArray as $model => $table){
-			if (count(DB::table($table)->get()) != 0) {
-				//Turn foreign key checks off <- USE WITH CAUTION!
-				DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-				//Delete all entries & reset indexes
-				DB::table($table)->truncate();
-				//Turn foreign key checks on <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
-				DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-			}
-		}
-		
-		//Assert all tables are empty
-		foreach ($modelTableArray as $model => $table){
-			$this->assertCount(0, DB::table($table)->get(), $table." table should be empty!");
-		}
-	}
-	
-	
-	
-	/**
-	 * Private helper to quickly clear tables without models
-	 * This function will also assert that all of the tables are empty
-	 * @param array $tableArray to clear
-	 */
-	private function clearTables(array $tableArray){
-		//Truncate every table in the array
-		foreach ($tableArray as $table){			
-			//Turn foreign key checks off <- USE WITH CAUTION!
-			DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-			//Delete all entries & reset indexes
-			DB::table($table)->truncate();
-			//Turn foreign key checks on <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
-			DB::statement('SET FOREIGN_KEY_CHECKS=1;');			
-		}
-		
-		//Assert all tables are empty
-		foreach ($tableArray as $table){
-			$this->assertCount(0, DB::table($table)->get(), $table." table should be empty!");
-		}
-	}
-	
-	
-	
-	/**
-	 * Drops & resets any event listeners associated with models.
-	 * Called in the <code>TestCase</code> <code>setUp()</code> function
-	 * Workaround for an issue when testing: https://github.com/laravel/framework/issues/1181
-	 */
-	private function refreshListeners(){
-		//Reset event listeners on all models
-		foreach (TestSettings::$modelTables as $model => $table){			
-			call_user_func(array($model, 'flushEventListeners'));			
-			call_user_func(array($model, 'boot'));
-		}
-		foreach (TestSettings::$seededModelTables as $model => $table){			
-			call_user_func(array($model, 'flushEventListeners'));			
-			call_user_func(array($model, 'boot'));
-		}		
-	}
-	
 	
 	
 	/**
@@ -178,7 +39,7 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase {
 	 */
 	public function createApplication()
 	{
-		$unitTesting = true;	
+		$unitTesting = true;
 		$testEnvironment = 'testing';	
 		return require __DIR__.'/../../bootstrap/start.php';
 	}
--- app/tests/TestHelper.php
+++ app/tests/TestHelper.php
@@ -5,6 +5,180 @@
  */
 class TestHelper{
 		
+	/**
+	 * Helper function to call any needed migrations on the test database
+	 */
+	public static function dbMigrate(){
+		if (App::runningUnitTests()) {
+			echo("\nDB ".getenv('DATABASE_NAME').": Migrating.....");
+			//Migrate the database
+			Artisan::call('migrate');
+		}
+	}
 	
+	
+	
+	/**
+	 * Helper function to clear all unseeded tables in the test database (including pivots)
+	 */
+	public static function dbClear(){
+		if (App::runningUnitTests()) {
+			echo("\nDB ".getenv('DATABASE_NAME').": Clearing tables.....");
+			self::clearModelTables(TestSettings::$modelTables);
+			self::clearTables(TestSettings::$pivotTables);
+		}
+	}
+	
+	
+	
+	/**
+	 * Helper function to clear a table in the test database (including pivots)
+	 * @param string $table the name of the table to be
+	 */
+	public static function dbClearTable($table){
+		if (App::runningUnitTests()) {
+			echo("\nDB ".getenv('DATABASE_NAME').": Clearing ".$table." table.....");
+			if (count(DB::table($table)->get()) != 0) {
+				//Turn foreign key checks off <- USE WITH CAUTION!
+				DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+				//Delete all entries & reset indexes
+				DB::table($table)->truncate();
+				//Turn foreign key checks on <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
+				DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Helper function to clear and reseed all seeded tables in the test database
+	 */
+	public static function dbSeed(){
+		if (App::runningUnitTests()) {
+			echo("\nDB ".getenv('DATABASE_NAME').": Seeding.....");
+			//Turn foreign key checks off
+			DB::statement('SET FOREIGN_KEY_CHECKS=0;');// <- USE WITH CAUTION!
+			//Seed tables
+			Artisan::call('db:seed');
+			//Turn foreign key checks on
+			DB::statement('SET FOREIGN_KEY_CHECKS=1;');// <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
+		}
+	}
+	
+	
+	
+	/**
+	 * Helper function to clear and reseed a single seeded table in the test database
+	 * @param string $seederPrefix first part of the seeder class name (eg PREFIXTableSeeder)
+	 */
+	public static function dbSeedTable($seederPrefix){
+		if (App::runningUnitTests()) {
+			echo("\nDB ".getenv('DATABASE_NAME').": Seeding ".$seederPrefix.".....");
+			//Turn foreign key checks off
+			DB::statement('SET FOREIGN_KEY_CHECKS=0;');// <- USE WITH CAUTION!
+			//Seed tables
+			Artisan::call('db:seed', array('--class='.$seederPrefix.'TableSeeder'));
+			//Turn foreign key checks on
+			DB::statement('SET FOREIGN_KEY_CHECKS=1;');// <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
+		}
+	}
+	
+	
+	
+	/**
+	 * Helper function to set up a 'clean' test database
+	 * It will do the following:
+	 * Reset all migrations (optional)
+	 * Perform any required migrations
+	 * Clear all unseeded & pivot tables
+	 * Reseed any seeded tables
+	 * @param bool $includeReset flag to set whether or not rollback all migrations first
+	 */
+	public static function dbRefresh($includeReset = false){
+		if (App::runningUnitTests()) {
+			echo("\nDB ".getenv('DATABASE_NAME').": Refreshing.....");
+				
+			//@TODO Fix issue with rollbacks before this can be used
+			// 		if ($includeReset) {
+			// 			echo("\nDB ".getenv('DATABASE_NAME').": Rolling back migrations.....");
+			// 			//Turn foreign key checks off <- USE WITH CAUTION!
+			// 			DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+			// 			//Rollback all migrations
+			// 			Artisan::call('migrate:reset');
+			// 			//Turn foreign key checks on <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
+			// 			DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+			// 		}
+	
+			self::dbMigrate();
+			self::dbClear();
+			self::dbSeed();
+		}
+	}
+	
+	
+	
+	/**
+	 * Private helper to quickly clear tables with models
+	 * This function will also assert that all of the tables are empty
+	 * @param array $modelTableArray to clear
+	 */
+	private static function clearModelTables(array $modelTableArray){
+		if (App::runningUnitTests()) {
+			//Get count for each table & truncate it if its not empty
+			foreach ($modelTableArray as $model => $table){
+				if (count(DB::table($table)->get()) != 0) {
+					//Turn foreign key checks off <- USE WITH CAUTION!
+					DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+					//Delete all entries & reset indexes
+					DB::table($table)->truncate();
+					//Turn foreign key checks on <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
+					DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+				}
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Private helper to quickly clear tables without models
+	 * This function will also assert that all of the tables are empty
+	 * @param array $tableArray to clear
+	 */
+	private static function clearTables(array $tableArray){
+		if (App::runningUnitTests()) {
+			//Truncate every table in the array
+			foreach ($tableArray as $table){
+				//Turn foreign key checks off <- USE WITH CAUTION!
+				DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+				//Delete all entries & reset indexes
+				DB::table($table)->truncate();
+				//Turn foreign key checks on <- SHOULD RESET ANYWAY BUT JUST TO MAKE SURE!
+				DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+			}
+		}
+	}
+	
+	
+	
+	/**
+	 * Drops & resets any event listeners associated with models.
+	 * Called in the <code>TestCase</code> <code>setUp()</code> function
+	 * Workaround for an issue when testing: https://github.com/laravel/framework/issues/1181
+	 */
+	public static function refreshListeners(){
+		if (App::runningUnitTests()) {
+			//Reset event listeners on all models
+			foreach (TestSettings::$modelTables as $model => $table){
+				call_user_func(array($model, 'flushEventListeners'));
+				call_user_func(array($model, 'boot'));
+			}
+			foreach (TestSettings::$seededModelTables as $model => $table){
+				call_user_func(array($model, 'flushEventListeners'));
+				call_user_func(array($model, 'boot'));
+			}
+		}
+	}
 	
 }
