07076eef2cea45b8b689498800fafc452178bd4c
Soren Schwert <soren@scubawhere.com>
2014-10-27 02:11:39 +0000

    First iteration of new `Locations` tab
    References #34.
    Supports distinguished display of attached vs. available locations.
    No interaction support yet.

--- public_html/dashboard/tabs/locations/js/script.js
+++ public_html/dashboard/tabs/locations/js/script.js
@@ -1,203 +1,122 @@
-var map;
+var gmap = promises = {};
 
-if(!window.token) {
-	$.get("/token", null, function(data) {
-		window.token = data;
+$(function() {
+	console.log('Setting container height');
+	// Set up height of map container to fill whole screen whithout showing scrollbars
+	$('#map-container').css('height', window.innerHeight - 232);
+
+	loadGoogleMaps();
+
+});
+
+function initialise() {
+	console.log('Initialising started');
+	var mapOptions = {
+		zoom:   5,
+		center: new google.maps.LatLng(company.latitude, company.longitude)
+		// center: new google.maps.LatLng(50.582847, 5.96848) // Somewhere around Aachen in Germany
+	};
+
+	window.gmap = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+	// Load locations
+
+	window.promises.loadedLocations         = $.Deferred();
+	window.promises.loadedAttachedLocations = $.Deferred();
+
+	google.maps.event.addListenerOnce(gmap, 'bounds_changed', function() {
+		loadLocationsInView();
+		loadAttachedLocations();
+	});
+
+	promises.loadedLocations.done(function() {
+		promises.loadedAttachedLocations.done(function() {
+			// Both calls have finished and we can get to work
+			console.log('All locations loaded');
+
+			removeAttachedFromLocations();
+
+			// Render all locations
+			renderLocations();
+			renderAttachedLocations();
+		});
 	});
 }
 
-$(function() {
+function loadLocationsInView(promise) {
+	var bounds = window.gmap.getBounds();
+	var north  = bounds.getNorthEast().lat(),
+	    west   = bounds.getSouthWest().lng(),
+	    south  = bounds.getSouthWest().lat(),
+	    east   = bounds.getNorthEast().lng();
 
-	// Update position
-	$(document).on('submit', '.edit_marker', function(e) {
-		e.preventDefault();
-
-		var $index = $(this).data('marker-index');
-
-		$lat = $('#marker_' + $index + '_lat').val();
-		$lng = $('#marker_' + $index + '_lng').val();
-
-		var template = $('#edit_marker_template').text();
-
-		// Update form values
-		var content = template.replace(/{{index}}/g, $index).replace(/{{lat}}/g, $lat).replace(/{{lng}}/g, $lng);
-
-		map.markers[$index].setPosition(new google.maps.LatLng($lat, $lng));
-		map.markers[$index].infoWindow.setContent(content);
-
-		$marker = $('#markers-with-coordinates').find('li').eq(0).find('a');
-		$marker.data('marker-lat', $lat);
-		$marker.data('marker-lng', $lng);
-	});
-
-	// Update center
-	$(document).on('click', '.pan-to-marker', function(e) {
-		e.preventDefault();
-
-		var lat, lng;
-
-		var $index = $(this).data('marker-index');
-		var $lat = $(this).data('marker-lat');
-		var $lng = $(this).data('marker-lng');
-
-		if ($index != undefined) {
-			// using indices
-			var position = map.markers[$index].getPosition();
-			lat = position.lat();
-			lng = position.lng();
-		}
-		else {
-			// using coordinates
-			lat = $lat;
-			lng = $lng;
-		}
-
-		map.setCenter(lat, lng);
-	});
-
-	map = new GMaps({
-		div: '#map',
-		lat: 0,
-		lng: 0,
-		width: '100%',
-		height: '400px',
-		zoom: 8
-	});
-
-	google.maps.event.addListener(map.map, 'bounds_changed', function() {
-		// Trigger the map resize after the map has loaded, to fix the map not showing properly
-		// console.log("Resize now!");
-		google.maps.event.trigger(map.map, 'resize');
-	});
-
-	GMaps.geolocate({
-		success: function(position){
-			map.setCenter(position.coords.latitude, position.coords.longitude);
-		}
-		//,error: function(error){
-		//alert('Geolocation failed: '+error.message);
-		//},
-		//not_supported: function(){
-		// alert("Your browser does not support geolocation");
-		//},
-		//always: function(){
-		//alert("Done!");
-		//}
-	});
-
-	$('#tags').tagsInput({width:'502px', height: "60px"});
-
-	_token = window.token || $.ajax({
-		url: "/token",
-		type: "GET",
-		dataType: "html",
-		async: false,
-		success: function(data){
-			$("[name='_token']").val(data)
-		}
-	});
-
-	/*
-	var locSource = $("#location").html();
-	var locTemplate = Handlebars.compile(locSource);
-	*/
-
-	var locationSource = $("#location-list-template").html();
-	var locationTemplate = Handlebars.compile(locationSource);
-
-	Place.getLocationsAround({latitude: 0, longitude: 0, limit: 9999}, function success(data) {
-		$.each(data, function(){
-			//$("#locations").append(locTemplate(this));
-			map.addMarker({
-				lat: this.latitude,
-				lng: this.longitude,
-				title: this.name,
-				color: "blue",
-				icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-			});
-		});
-
-		Place.getAttachedLocations(function success(data) {
-			$.each(data, function(){
-				 //$("#locations").append(locTemplate(this));
-				map.addMarker({
-					lat: this.latitude,
-					lng: this.longitude,
-					title: this.name,
-					color: "red",
-					icon: "http://maps.google.com/mapfiles/ms/icons/red-dot.png"
-				});
-
-				$("#locations").append(locationTemplate(this));
-			});
-		});
-	});
-
-	/*Locations.getAttachedLocations(function success(data){
-		$.each(data, function(){
-				/* $("#locations").append(locTemplate(this));
-				map.addMarker({
-					  lat: this.latitude,
-					  lng: this.longitude,
-					  title: this.name,
-					  icon: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
-				  });
-
-				$("#locations").append(locationTemplate(this));
-	});
-	});*/
-
-	var newMarker;
-
-	/* 	ADD MARKER */
-	GMaps.on('click', map.map, function(event) {
-		var index = map.markers.length;
-		var lat = event.latLng.lat();
-		var lng = event.latLng.lng();
-
-		$("[name='latitude']").val(lat);
-		$("[name='longitude']").val(lng);
-
-		//map.removeMarker('Marker #' + (index - 1));
-		map.removeMarkers();
-
-		var template = $('#edit_marker_template').text();
-
-		var content = template.replace(/{{index}}/g, index).replace(/{{lat}}/g, lat).replace(/{{lng}}/g, lng);
-
-		map.addMarker({
-			lat: lat,
-			lng: lng,
-			title: 'Marker #' + index,
-			infoWindow: {
-				content : content
-			},
-			icon: "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
-		});
-	});
-
-	$("form#save-location").submit(function(event) {
-		event.preventDefault();
-
-		Place.create( $("form#save-location").serialize(), function success(data){
-			pageMssg(data.status, true);
-			console.log(data);
-		});
-	});
-});
-
-function detachLocation(id) {
-
+	// var area   = [north, west, south, east];
+	// TODO make the location creating relative, meaning render only locations in view and which are not on the map yet
+	var area = [-90, -180, 90, 180];
 	var params = {
-		_token : window.token,
-		id : id
+		'area': area
 	};
+	Place.inside(params, function success(data) {
+		window.locations = _.indexBy(data, 'id');
 
-	console.log(id);
+		promises.loadedLocations.resolve();
+	});
+}
+function loadAttachedLocations(promise) {
 
-	Place.detach(params, function success(data) {
-		pageMssg(data.status, true);
+	Place.attached(function success(data) {
+		window.attachedLocations = _.indexBy(data, 'id');
+
+		promises.loadedAttachedLocations.resolve();
+	});
+}
+
+function removeAttachedFromLocations() {
+	// Remove all attached locations from the general locations collection
+	window.locations = _.omit(window.locations, _.keys(window.attachedLocations));
+}
+
+function renderLocations() {
+	var markerArray = [];
+	_.each(window.locations, function(location) {
+		var markerOptions = {
+			position:  new google.maps.LatLng( location.latitude, location.longitude ),
+			// map:       gmap,
+			title:     location.name,
+			animation: google.maps.Animation.DROP,
+			icon:      'http://mt.googleapis.com/vt/icon?color=ff004C13&name=icons/spotlight/spotlight-waypoint-blue.png&scale=1'
+		};
+		markerArray.push( new google.maps.Marker(markerOptions) );
 	});
 
-	// e.preventDefault();
+	var i = 0;
+	_.each(markerArray, function(marker) {
+		setTimeout(function() {
+			marker.setMap(gmap);
+		}, i * 400);
+		i++;
+	});
+}
+
+function renderAttachedLocations() {
+	console.log(window.attachedLocations);
+	var markerArray = [];
+	_.each(window.attachedLocations, function(location) {
+		var markerOptions = {
+			position:  new google.maps.LatLng( location.latitude, location.longitude ),
+			// map:       gmap,
+			title:     location.name,
+			animation: google.maps.Animation.DROP,
+			icon:      'http://mt.googleapis.com/vt/icon/name=icons/spotlight/spotlight-poi.png&scale=1'
+		};
+		markerArray.push( new google.maps.Marker(markerOptions) );
+	});
+
+	var i = 0;
+	_.each(markerArray, function(marker) {
+		setTimeout(function() {
+			marker.setMap(gmap);
+		}, i * 400);
+		i++;
+	});
 }
