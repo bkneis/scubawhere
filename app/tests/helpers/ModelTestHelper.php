a5b14a4671b4118a77ea66596cf104c1fa1a82a4
Jon Hiden <j_hiden@hotmail.com>
2014-11-06 21:27:22 +0000

    Added more create helpers

--- app/tests/helpers/ModelTestHelper.php
+++ app/tests/helpers/ModelTestHelper.php
@@ -7,66 +7,158 @@ class ModelTestHelper{
 
 	//Constants that can be used to quickly create & assert models
 
-	const TEST_STRING             = "Test string";
-	const TEST_STRING_UPDATED     = "New test string";
-	const TEST_ABBR               = "TST";
-	const TEST_ABBR_UPDATED       = "NTST";
+	const TEST_STRING = "Test string";
+	const TEST_STRING_UPDATED = "New test string";
+	const TEST_INTEGER = 100;
+	const TEST_INTEGER_UPDATED = 200;
+	const TEST_DECIMAL = 1.1;
+	const TEST_DECIMAL_UPDATED = 2.2;
+	const TEST_ABBR = "TST";
+	const TEST_ABBR_UPDATED = "NTS";
 	const TEST_SYMBOL = "£";
 	const TEST_SYMBOL_UPDATED = "$";
-	const TEST_USERNAME           = "testuser";
-	const TEST_PASSWORD           = "testpassword";
-	const TEST_EMAIL              = "test@email.com";
-	const TEST_DATE               = "2000-01-01 12:34:56";
+	const TEST_BOOL = false;
+	const TEST_BOOL_UPDATED = true;
+	const TEST_USERNAME = "testuser";
+	const TEST_USERNAME_UPDATED = "newtestuser";
+	const TEST_PASSWORD = "testpassword";
+	const TEST_PASSWORD_UPDATED = "newtestpassword";
+	const TEST_EMAIL = "test@email.com";
+	const TEST_EMAIL_UPDATED = "newtest@email.com";
+	const TEST_DATE = "2000-01-01 12:34:56";
+	const TEST_DATE_UPDATED = "2001-01-01 12:34:56";
+	const TEST_URL = "www.testurl.com";
+	const TEST_URL_UPDATED = "www.newtesturl.com";
+	const TEST_ADDRESS = "123 Test Lane, Aplace, Somewhere, ABC 123";
+	const TEST_ADDRESS_UPDATED = "456 Test Lane, Aplace, Somewhere, ABC 456";
+	const TEST_PHONE = "07123 456 789";
+	const TEST_PHONE_UPDATED = "07456 789 123";
+	const TEST_REFERENCE = "ABCD1234";
+	const TEST_REFERENCE_UPDATED = "EFGH5678";
 
 	//Create valid entries for each model & return its id
 	//Each function also supplies a way to append data
 	//to quickly create models with different values.
 
 	public static function createAccommodation($company_id, $append = ""){
-		//TODO
-		return 0;
+		$entry = new Accommodation();
+		
+		$entry->company_id = $company_id;
+		
+		$entry->name = self::TEST_STRING.$append;
+		$entry->description = self::TEST_STRING.$append;
+		$entry->photo = self::TEST_STRING.$append;
+		
+		$entry->save();
+		return $entry->id;
 	}
 
 	public static function createAddon($company_id, $append = ""){
-		//TODO
-		return 0;
+		$entry = new Addon();
+		
+		$entry->company_id = $company_id;
+		
+		$entry->name = self::TEST_STRING.$append;
+		$entry->description = self::TEST_STRING.$append;
+		$entry->price = self::TEST_INTEGER;
+		$entry->curreny = self::TEST_ABBR;
+		$entry->compulsory = self::TEST_BOOL;
+		
+		$entry->save();
+		return $entry->id;
 	}
 
 	public static function createAgency($append = ""){
 		$entry = new Agency();
+		
 		$entry->abbreviation = self::TEST_ABBR;
 		$entry->name = self::TEST_STRING.$append;
 		$entry->description = self::TEST_STRING.$append;
+		
 		$entry->save();
 		return $entry->id;
 	}
 
 	public static function createAgent($company_id, $append = ""){
-		//TODO
-		return 0;
+		$entry = new Agent();
+		
+		$entry->company_id = $company_id;
+		
+		$entry->name = self::TEST_STRING.$append;
+		$entry->website = self::TEST_URL;
+		$entry->branch_name = self::TEST_STRING.$append;
+		$entry->branch_address = self::TEST_ADDRESS;
+		$entry->branch_phone = self::TEST_PHONE;
+		$entry->branch_email = self::TEST_EMAIL;
+		$entry->billing_address = self::TEST_ADDRESS;
+		$entry->billing_phone = self::TEST_PHONE;
+		$entry->billing_email = self::TEST_EMAIL;
+		$entry->commission = self::TEST_DECIMAL;
+		$entry->terms = self::TEST_STRING;
+		
+		$entry->save();
+		return $entry->id;
 	}
 
 	public static function createBoat($company_id, $append = ""){
-		//TODO
-		return 0;
+		$entry = new Boat();
+		
+		$entry->company_id = $company_id;
+		
+		$entry->name = self::TEST_STRING.$append;
+		$entry->description = self::TEST_STRING.$append;
+		$entry->capacity = self::TEST_INTEGER;
+		$entry->photo = self::TEST_STRING.$append;
+		
+		$entry->save();
+		return $entry->id;
 	}
 
-	public static function createBoooking($company_id, $agent_id, $append = ""){
-		//TODO
-		return 0;
+	public static function createBooking($company_id, $agent_id, $append = ""){
+		$entry = new Booking();
+		
+		$entry->company_id = $company_id;
+		$entry->agent_id = $agent_id;
+		
+		$entry->reference = self::TEST_REFERENCE;
+		$entry->source = self::TEST_STRING.$append;
+		$entry->price = self::TEST_INTEGER;
+		$entry->curreny = self::TEST_ABBR;
+		$entry->discount = self::TEST_INTEGER;
+		$entry->confirmed = self::TEST_BOOL;
+		$entry->reserved = self::TEST_DATE;		
+		$entry->pick_up_location = self::TEST_ADDRESS;
+		$entry->pick_up_time = self::TEST_DATE;		
+		$entry->comment = self::TEST_STRING.$append;
+		
+		$entry->save();
+		return $entry->id;
 	}
 
-	public static function createBoookingdetail($booking_id, $customer_id, $append = ""){
-		//TODO
-		return 0;
+	public static function createBoookingdetail($booking_id, $customer_id, $ticket_id, $session_id, $packagefacade_id){
+		$entry = new Bookingdetail();
+		
+		$entry->booking_id = $booking_id;
+		$entry->customer_id = $customer_id;
+		$entry->ticket_id = $ticket_id;
+		$entry->session_id = $session_id;
+		$entry->packagefacade_id = $packagefacade_id;
+		
+		$entry->is_lead = self::TEST_BOOL;
+		
+		$entry->save();
+		return $entry->id;
 	}
 
 	public static function createCertificate($agency_id, $append = ""){
 		$entry = new Certificate();
+		
 		$entry->agency_id = $agency_id;
+		
 		$entry->abbreviation = self::TEST_ABBR;
 		$entry->name = self::TEST_STRING.$append;
 		$entry->description = self::TEST_STRING.$append;
+		
 		$entry->save();
 		return $entry->id;
 	}
@@ -78,31 +170,38 @@ class ModelTestHelper{
 
 	public static function createContinent($append = ""){
 		$entry = new Continent();
+		
 		$entry->abbreviation = self::TEST_ABBR;
 		$entry->name = self::TEST_STRING.$append;
 		$entry->description = self::TEST_STRING.$append;
+		
 		$entry->save();
 		return $entry->id;
 	}
 
 	public static function createCountry($continent_id, $currency_id, $append = ""){
 		$entry = new Country();
+		
 		$entry->continent_id = $continent_id;
 		$entry->currency_id = $currency_id;
+		
 		$entry->abbreviation = self::TEST_ABBR;
 		$entry->name = self::TEST_STRING.$append;
 		$entry->description = self::TEST_STRING.$append;
 		$entry->flag = self::TEST_STRING.$append;
+		
 		$entry->save();
 		return $entry->id;
 	}
 
 	public static function createCurrency($append = ""){
 		$entry = new Currency();
+		
 		$entry->code = self::TEST_ABBR;
 		$entry->name = self::TEST_STRING.$append;
 		$entry->description = self::TEST_STRING.$append;
 		$entry->symbol = self::TEST_SYMBOL.$append;
+		
 		$entry->save();
 		return $entry->id;
 	}
@@ -139,7 +238,9 @@ class ModelTestHelper{
 
 	public static function createPaymentgateway($append = ""){
 		$entry = new Paymentgateway();
+		
 		$entry->name = self::TEST_STRING.$append;
+		
 		$entry->save();
 		return $entry->id;
 	}
@@ -161,8 +262,10 @@ class ModelTestHelper{
 
 	public static function createTriptype($append = ""){
 		$entry = new Triptype();
+		
 		$entry->name = self::TEST_STRING.$append;
 		$entry->description = self::TEST_STRING.$append;
+		
 		$entry->save();
 		return $entry->id;
 	}
