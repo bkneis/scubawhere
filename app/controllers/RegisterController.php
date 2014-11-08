c8ce77182e6b6d9733b7e806f68d9d8ed504cda1
Soren Schwert <soren@scubawhere.com>
2014-11-08 18:48:36 +0000

    Adds LatLng lookup to registration process
    Having the lat & lng in the company table allows us to display the map on the locations tab more quickly, because we do not first need to do an address lookup.

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -6,6 +6,43 @@ class RegisterController extends Controller {
 	public function postCompany()
 	{
 		$data = Input::only('username', 'email', 'name', 'address_1', 'address_2', 'city', 'county', 'postcode', 'country_id', 'currency_id', 'business_phone', 'business_email', 'vat_number', 'registration_number', 'phone', 'website');
+
+		try
+		{
+			if( !Input::get('country_id') ) throw new ModelNotFoundException();
+			$country = Country::findOrFail( Input::get('country_id') );
+		}
+		catch(ModelNotFoundException $e)
+		{
+			return Response::json( array('errors' => array('The country could not be found.')), 404 ); // 404 Not Found
+		}
+
+		$address = implode( ',', array(
+			$data['address_1'],
+			$data['address_2'],
+			$data['postcode'],
+			$data['city'],
+			$data['county'],
+			$country->name,
+		) );
+
+		$ch = curl_init( 'https://maps.googleapis.com/maps/api/geocode/json?address='.$address );
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		$result = curl_exec( $ch );
+		curl_close( $ch );
+		$result = json_decode( $result );
+
+		if($result->status === "OK")
+		{
+			$data['latitude']  = $result->results[0]->geometry->location->lat;
+			$data['longitude'] = $result->results[0]->geometry->location->lng;
+		}
+		else
+		{
+			$data['latitude']  = 0;
+			$data['longitude'] = 0;
+		}
+
 		$company = new Company($data);
 
 		// Mass assigned insert with automatic validation
