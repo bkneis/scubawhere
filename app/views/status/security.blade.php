1e2d1030a2a70cee954db88ac7ffa8a22db8cb4c
Soren Schwert <soren@scubawhere.com>
2014-11-20 15:28:05 +0000

    Changes to status page security form

--- app/views/status/security.blade.php
+++ app/views/status/security.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 	<form method="post" role="form" class="form-inline text-center">
+		<h1><i class="fa fa-lock"></i></h1>
 		<div class="input-group">
-			<input type="password" name="password" class="form-control" autofocus>
-			<span class="input-group-addon"><i class="fa fa-lock"></i></span>
+			<input type="password" name="password" class="form-control text-center" autofocus>
 		</div>
 	</form>
 @stop
