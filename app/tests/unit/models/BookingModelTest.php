783cd923b23376b6541b4bc5c495ae2b0d25d3c4
Soren Schwert <soren@scubawhere.com>
2014-11-23 16:40:50 +0000

    Fixes problem with Booking requiring a logged in user to convert the discount attribute

--- app/tests/unit/models/BookingModelTest.php
+++ app/tests/unit/models/BookingModelTest.php
@@ -1,26 +1,29 @@
 <?php
 
 class BookingModelTest extends ModelTestCase {
-		
+
 	public function setUp()
-	{		
-		parent::setUp();		
+	{
+		parent::setUp();
 	}
-	
+
 	public function testCRUD(){
-		
-		//Create/Read		
+
+		//Create/Read
 		$continent_id = ModelTestHelper::createContinent();
 		$currency_id = ModelTestHelper::createCurrency();
 		$country_id = ModelTestHelper::createCountry($continent_id, $currency_id);
 		$company_id = ModelTestHelper::createCompany($country_id, $currency_id);
+		//We must have an authenticated Company
+		$company = Company::find($company_id);
+		$this->be($company);
 		$agent_id = ModelTestHelper::createAgent($company_id);
 		$booking_id = ModelTestHelper::createBooking($company_id, $agent_id);
 		$booking = Booking::find($booking_id);
-		
+
 		$this->assertNotEquals(0, $booking->id, "Unexpected id value");
 		$this->assertEquals($company_id, $booking->company_id, "Unexpected id value");
-		$this->assertEquals($agent_id, $booking->agent_id, "Unexpected id value");		
+		$this->assertEquals($agent_id, $booking->agent_id, "Unexpected id value");
 		$this->assertEquals(ModelTestHelper::TEST_REFERENCE, $booking->reference, "Unexpected reference value");
 		$this->assertEquals(ModelTestHelper::TEST_SOURCE, $booking->source, "Unexpected source value");
 		$this->assertEquals(ModelTestHelper::TEST_INTEGER, $booking->price, "Unexpected price value");
@@ -32,8 +35,8 @@ class BookingModelTest extends ModelTestCase {
 		$this->assertEquals(ModelTestHelper::TEST_STRING, $booking->comment, "Unexpected comment value");
 		$this->assertNotEquals("0000-00-00 00:00:00", $booking->created_at);
 		$this->assertNotEquals("0000-00-00 00:00:00", $booking->updated_at);
-				
-		//Update		
+
+		//Update
 		$booking->reference = ModelTestHelper::TEST_REFERENCE_UPDATED;
 		$booking->source = ModelTestHelper::TEST_SOURCE_UPDATED;
 		$booking->price = ModelTestHelper::TEST_INTEGER_UPDATED;
@@ -43,7 +46,7 @@ class BookingModelTest extends ModelTestCase {
 		$booking->pick_up_location = ModelTestHelper::TEST_ADDRESS_UPDATED;
 		$booking->pick_up_time = ModelTestHelper::TEST_DATE_UPDATED;
 		$booking->comment = ModelTestHelper::TEST_STRING_UPDATED;
-		
+
 		$this->assertNotEquals(0, $booking->id, "Unexpected id value");
 		$this->assertEquals(ModelTestHelper::TEST_REFERENCE_UPDATED, $booking->reference, "Unexpected reference value");
 		$this->assertEquals(ModelTestHelper::TEST_SOURCE_UPDATED, $booking->source, "Unexpected source value");
@@ -54,31 +57,34 @@ class BookingModelTest extends ModelTestCase {
 		$this->assertEquals(ModelTestHelper::TEST_ADDRESS_UPDATED, $booking->pick_up_location, "Unexpected pick_up_location value");
 		$this->assertEquals(ModelTestHelper::TEST_DATE_UPDATED, $booking->pick_up_time, "Unexpected pick_up_time value");
 		$this->assertEquals(ModelTestHelper::TEST_STRING_UPDATED, $booking->comment, "Unexpected comment value");
-				
+
 		//Delete
 		$booking->delete();
 		$booking = Booking::find($booking_id);
-		
+
 		$this->assertNull($booking, "Booking not deleted");
 	}
-	
+
 	public function testValidation(){
 		$this->markTestIncomplete('This test needs to be completed!');
 	}
-	
+
 	public function testRelationships(){
 		$continent_id = ModelTestHelper::createContinent();
 		$currency_id = ModelTestHelper::createCurrency();
 		$country_id = ModelTestHelper::createCountry($continent_id, $currency_id);
 		$company_id = ModelTestHelper::createCompany($country_id, $currency_id);
+		//We must have an authenticated Company
+		$company = Company::find($company_id);
+		$this->be($company);
 		$agent_id = ModelTestHelper::createAgent($company_id);
 		$booking_id = ModelTestHelper::createBooking($company_id, $agent_id);
 		$booking = Booking::find($booking_id);
-	
+
 		$this->assertNotNull($booking->company, "Unexpected company relationship value");
 		$this->assertNotNull($booking->agent, "Unexpected agent relationship value");
 	}
-	
+
 	public function testFunctions(){
 		//We must have an authenticated Company to grab its currency value
 		$this->be(TestHelper::createAuthenticationCompany());
@@ -93,9 +99,9 @@ class BookingModelTest extends ModelTestCase {
 		
 		$this->assertEquals("0.00", $booking->decimal_price, "Unexpected decimal_price value");//0.00 due to discount
 	}
-	
+
 	public function testEdges(){
 		$this->assertTrue(true);
-	}	
-	
+	}
+
 }
--- app/tests/unit/models/BookingdetailModelTest.php
+++ app/tests/unit/models/BookingdetailModelTest.php
@@ -1,95 +1,101 @@
 <?php
 
 class BookingdetailModelTest extends ModelTestCase {
-		
+
 	public function setUp()
-	{		
-		parent::setUp();		
+	{
+		parent::setUp();
 	}
-	
+
 	public function testCRUD(){
-		
+
 		//Create/Read
 		$continent_id = ModelTestHelper::createContinent();
 		$currency_id = ModelTestHelper::createCurrency();
 		$country_id = ModelTestHelper::createCountry($continent_id, $currency_id);
 		$company_id = ModelTestHelper::createCompany($country_id, $currency_id);
+		//We must have an authenticated Company
+		$company = Company::find($company_id);
+		$this->be($company);
 		$agent_id = ModelTestHelper::createAgent($company_id);
 		$location_id = ModelTestHelper::createLocation();
 		$trip_id = ModelTestHelper::createTrip($company_id, $location_id);
 		$boat_id = ModelTestHelper::createBoat($company_id);
 		$timetable_id = ModelTestHelper::createTimetable($company_id);
 		$package_id = ModelTestHelper::createPackage($company_id);
-		
+
 		$booking_id = ModelTestHelper::createBooking($company_id, $agent_id);
 		$customer_id = ModelTestHelper::createCustomer($country_id, $company_id);
 		$ticket_id = ModelTestHelper::createTicket($company_id);
 		$session_id = ModelTestHelper::createDeparture($trip_id, $boat_id, $timetable_id);
-		$packagefacade_id = ModelTestHelper::createPackagefacade($package_id);		
+		$packagefacade_id = ModelTestHelper::createPackagefacade($package_id);
 		$bookingdetail_id = ModelTestHelper::createBookingdetail($booking_id, $customer_id, $ticket_id, $session_id, $packagefacade_id);
 		$bookingdetail = Bookingdetail::find($bookingdetail_id);
-		
+
 		$this->assertNotEquals(0, $bookingdetail->id, "Unexpected id value");
 		$this->assertEquals($booking_id, $bookingdetail->booking_id, "Unexpected id value");
 		$this->assertEquals($customer_id, $bookingdetail->customer_id, "Unexpected id value");
 		$this->assertEquals($ticket_id, $bookingdetail->ticket_id, "Unexpected id value");
 		$this->assertEquals($session_id, $bookingdetail->session_id, "Unexpected id value");
-		$this->assertEquals($packagefacade_id, $bookingdetail->packagefacade_id, "Unexpected id value");		
+		$this->assertEquals($packagefacade_id, $bookingdetail->packagefacade_id, "Unexpected id value");
 		$this->assertEquals(ModelTestHelper::TEST_BOOL, $bookingdetail->is_lead, "Unexpected is_lead value");
 		$this->assertNotEquals("0000-00-00 00:00:00", $bookingdetail->created_at);
 		$this->assertNotEquals("0000-00-00 00:00:00", $bookingdetail->updated_at);
-				
-		//Update		
+
+		//Update
 		$bookingdetail->is_lead = ModelTestHelper::TEST_BOOL_UPDATED;
-		$bookingdetail->save();		
+		$bookingdetail->save();
 		$bookingdetail = Bookingdetail::find($bookingdetail_id);
-		
-		$this->assertNotEquals(0, $bookingdetail->id, "Unexpected id value");		
+
+		$this->assertNotEquals(0, $bookingdetail->id, "Unexpected id value");
 		$this->assertEquals(ModelTestHelper::TEST_BOOL_UPDATED, $bookingdetail->is_lead, "Unexpected is_lead value");
-				
+
 		//Delete
 		$bookingdetail->delete();
-		$bookingdetail = Bookingdetail::find($bookingdetail_id);		
+		$bookingdetail = Bookingdetail::find($bookingdetail_id);
 		$this->assertNull($bookingdetail, "Bookingdetail not deleted");
 	}
-	
+
 	public function testValidation(){
 		$this->markTestIncomplete('This test needs to be completed!');
 	}
-	
+
 	public function testRelationships(){
 		$continent_id = ModelTestHelper::createContinent();
 		$currency_id = ModelTestHelper::createCurrency();
 		$country_id = ModelTestHelper::createCountry($continent_id, $currency_id);
 		$company_id = ModelTestHelper::createCompany($country_id, $currency_id);
+		//We must have an authenticated Company
+		$company = Company::find($company_id);
+		$this->be($company);
 		$agent_id = ModelTestHelper::createAgent($company_id);
 		$location_id = ModelTestHelper::createLocation();
 		$trip_id = ModelTestHelper::createTrip($company_id, $location_id);
 		$boat_id = ModelTestHelper::createBoat($company_id);
 		$timetable_id = ModelTestHelper::createTimetable($company_id);
 		$package_id = ModelTestHelper::createPackage($company_id);
-		
+
 		$booking_id = ModelTestHelper::createBooking($company_id, $agent_id);
 		$customer_id = ModelTestHelper::createCustomer($country_id, $company_id);
 		$ticket_id = ModelTestHelper::createTicket($company_id);
 		$session_id = ModelTestHelper::createDeparture($trip_id, $boat_id, $timetable_id);
-		$packagefacade_id = ModelTestHelper::createPackagefacade($package_id);		
+		$packagefacade_id = ModelTestHelper::createPackagefacade($package_id);
 		$bookingdetail_id = ModelTestHelper::createBookingdetail($booking_id, $customer_id, $ticket_id, $session_id, $packagefacade_id);
 		$bookingdetail = Bookingdetail::find($bookingdetail_id);
-	
+
 		$this->assertNotNull($bookingdetail->booking, "Unexpected booking relationship value");
 		$this->assertNotNull($bookingdetail->customer, "Unexpected customer relationship value");
 		$this->assertNotNull($bookingdetail->ticket, "Unexpected ticket relationship value");
 		$this->assertNotNull($bookingdetail->session, "Unexpected session relationship value");
 		$this->assertNotNull($bookingdetail->packagefacade, "Unexpected packagefacade relationship value");
 	}
-	
+
 	public function testFunctions(){
 		$this->assertTrue(true);
 	}
-	
+
 	public function testEdges(){
 		$this->assertTrue(true);
-	}	
-	
+	}
+
 }
