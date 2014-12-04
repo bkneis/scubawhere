e617d4f6fa8f3b8d36b74d4484e59229863f2557
Soren Schwert <soren@scubawhere.com>
2014-12-04 21:53:23 +0000

    Update deploy script
    [skip ci]

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,9 @@ EOT
     shortrevision=${revision:0:7}
 
     COMMENT=$( cat COMMENT | sed ':a;N;$!ba;s/\n/\\n/g' ) # Replace newlines with the string '\n'
-    curl -X POST --data-urlencode 'payload={"username":"Deployment failed!","icon_url":"https://pbs.twimg.com/profile_images/378800000469704057/647e5983f89f6052c5f234947edb2774.png","attachments":[{"fallback":"<https://scubawhere.dploy.io|Visit the Dashboard>to view the deployment log","pretext":"<https://scubawhere.dploy.io|Visit the Dashboard>to view the deployment log","color":"danger","fields":[{"title":"Message","value":"'"$shortrevision"': '" $COMMENT "'","short":false}]}]}' https://scubawhere.slack.com/services/hooks/incoming-webhook?token=qVHxaGtxuYL6a4mLLyPwVgsc
+    curl -X POST --data-urlencode 'payload={"username":"Deployment failed!","icon_url":"https://pbs.twimg.com/profile_images/378800000469704057/647e5983f89f6052c5f234947edb2774.png","attachments":[{"fallback":"<https://scubawhere.dploy.io|Visit the Dashboard> to view the deployment log","pretext":"<https://scubawhere.dploy.io|Visit the Dashboard> to view the deployment log","color":"danger","fields":[{"title":"Message","value":"'"$shortrevision"': '" $COMMENT "'","short":false}]}]}' https://scubawhere.slack.com/services/hooks/incoming-webhook?token=qVHxaGtxuYL6a4mLLyPwVgsc
+
+    exit
 }
 
 trap deploy_failure ERR
