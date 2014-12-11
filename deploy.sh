fff55e224c8b4fb30850f6d1df13a39166d09e80
Soren Schwert <soren@scubawhere.com>
2014-12-11 15:21:05 +0000

    Remove username from deploy-script
    [skip ci]

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ EOT
     shortrevision=${revision:0:7}
 
     COMMENT=$( cat COMMENT | sed ':a;N;$!ba;s/\n/\\n/g' ) # Replace newlines with the string '\n'
-    curl -X POST --data-urlencode 'payload={"username": "Dploy.io", "icon_url": "https://pbs.twimg.com/profile_images/378800000469704057/647e5983f89f6052c5f234947edb2774.png", "attachments":[{"fallback":"<https://bitbucket.org/scubawhere/system/commits/%REVISION%|'"$shortrevision"'> by %USER_NAME% has been deployed to %ENV_NAME%.", "pretext":"<https://bitbucket.org/scubawhere/system/commits/%REVISION%|'"$shortrevision"'> by %USER_NAME% has been deployed to %ENV_NAME%.", "color":"good", "fields":[{"title":"Deployment successful", "value":"'"$COMMENT"'", "short":false}]}]}' https://scubawhere.slack.com/services/hooks/incoming-webhook?token=qVHxaGtxuYL6a4mLLyPwVgsc
+    curl -X POST --data-urlencode 'payload={"username": "Dploy.io", "icon_url": "https://pbs.twimg.com/profile_images/378800000469704057/647e5983f89f6052c5f234947edb2774.png", "attachments":[{"fallback":"<https://bitbucket.org/scubawhere/system/commits/%REVISION%|'"$shortrevision"'> has been deployed to %ENV_NAME%.", "pretext":"<https://bitbucket.org/scubawhere/system/commits/%REVISION%|'"$shortrevision"'> has been deployed to %ENV_NAME%.", "color":"good", "fields":[{"title":"Deployment successful", "value":"'"$COMMENT"'", "short":false}]}]}' https://scubawhere.slack.com/services/hooks/incoming-webhook?token=qVHxaGtxuYL6a4mLLyPwVgsc
 }
 
 function deploy_failure()
@@ -23,7 +23,7 @@ EOT
     shortrevision=${revision:0:7}
 
     COMMENT=$( cat COMMENT | sed ':a;N;$!ba;s/\n/\\n/g' ) # Replace newlines with the string '\n'
-    curl -X POST --data-urlencode 'payload={"username":"Dploy.io","icon_url":"https://pbs.twimg.com/profile_images/378800000469704057/647e5983f89f6052c5f234947edb2774.png","attachments":[{"fallback":"<https://scubawhere.dploy.io|Visit the Dashboard> to view the deployment log","pretext":"<https://scubawhere.dploy.io|Visit the Dashboard> to view the deployment log","color":"danger","fields":[{"title":"Deployment failed","value":"'"$shortrevision"' by %USER_NAME%: '" $COMMENT "'","short":false}]}]}' https://scubawhere.slack.com/services/hooks/incoming-webhook?token=qVHxaGtxuYL6a4mLLyPwVgsc
+    curl -X POST --data-urlencode 'payload={"username":"Dploy.io","icon_url":"https://pbs.twimg.com/profile_images/378800000469704057/647e5983f89f6052c5f234947edb2774.png","attachments":[{"fallback":"<https://scubawhere.dploy.io|Visit the Dashboard> to view the deployment log","pretext":"<https://scubawhere.dploy.io|Visit the Dashboard> to view the deployment log","color":"danger","fields":[{"title":"Deployment failed","value":"'"$shortrevision"': '" $COMMENT "'","short":false}]}]}' https://scubawhere.slack.com/services/hooks/incoming-webhook?token=qVHxaGtxuYL6a4mLLyPwVgsc
 
     exit
 }
