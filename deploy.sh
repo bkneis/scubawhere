b432caa89061f056b77ae534547ad69e80811930
Soren Schwert <soren@scubawhere.com>
2015-02-06 14:41:24 +0000

    Remove Slack webhook from deploy script
    The Slack integration is now handled directly by dploy.io.

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,3 @@
-function deploy_success()
-{
-# Prepare Slack webhook
-cat << "EOT" > COMMENT
-%COMMENT%
-EOT
-
-    revision='%REVISION%'
-    shortrevision=${revision:0:7}
-
-    COMMENT=$( cat COMMENT | sed ':a;N;$!ba;s/\n/\\n/g' ) # Replace newlines with the string '\n'
-    curl -X POST --data-urlencode 'payload={"username": "Dploy.io", "icon_url": "https://pbs.twimg.com/profile_images/378800000469704057/647e5983f89f6052c5f234947edb2774.png", "attachments":[{"fallback":"<https://bitbucket.org/scubawhere/system/commits/%REVISION%|'"$shortrevision"'> has been deployed to %ENV_NAME%.", "pretext":"<https://bitbucket.org/scubawhere/system/commits/%REVISION%|'"$shortrevision"'> has been deployed to %ENV_NAME%.", "color":"good", "fields":[{"title":"Deployment successful", "value":"'"$COMMENT"'", "short":false}]}]}' https://scubawhere.slack.com/services/hooks/incoming-webhook?token=qVHxaGtxuYL6a4mLLyPwVgsc
-}
-
-function deploy_failure()
-{
-# Prepare Slack webhook
-cat << "EOT" > COMMENT
-%COMMENT%
-EOT
-
-    revision='%REVISION%'
-    shortrevision=${revision:0:7}
-
-    COMMENT=$( cat COMMENT | sed ':a;N;$!ba;s/\n/\\n/g' ) # Replace newlines with the string '\n'
-    curl -X POST --data-urlencode 'payload={"username":"Dploy.io","icon_url":"https://pbs.twimg.com/profile_images/378800000469704057/647e5983f89f6052c5f234947edb2774.png","attachments":[{"fallback":"<https://scubawhere.dploy.io|Visit the Dashboard> to view the deployment log","pretext":"<https://scubawhere.dploy.io|Visit the Dashboard> to view the deployment log","color":"danger","fields":[{"title":"Deployment failed","value":"'"$shortrevision"': '" $COMMENT "'","short":false}]}]}' https://scubawhere.slack.com/services/hooks/incoming-webhook?token=qVHxaGtxuYL6a4mLLyPwVgsc
-
-    exit
-}
-
-trap deploy_failure ERR
-
 # Manually start ssh-agent and add deployment key to it
 ssh-agent /bin/bash
 ssh-add ~/.ssh/id_bitbucket_krystal
@@ -42,5 +10,3 @@ exit
 yes | php -d disable_functions= artisan migrate
 
 php -d disable_functions= artisan optimize
-
-deploy_success
