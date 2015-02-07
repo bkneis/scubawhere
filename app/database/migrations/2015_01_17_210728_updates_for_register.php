7f056307d9edd3f0c61cfebcb8071b4c8d8c1efa
bkneis <bryan@iqwebcreations.com>
2015-02-07 15:11:23 +0000

    Addition of terms to company db table and extra diving organisations
    --skip-ci

---
+++ app/database/migrations/2015_01_17_210728_updates_for_register.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAgenciesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::table('agencies')->insert(
+            array(
+                    array('abbreviation' => 'HSA'),
+                    array('abbreviation' => 'RAID'),
+                    array('abbreviation' => 'other'),                                
+            )
+        );
+
+        Schema::table('companies', function($table){
+		    $table->string('terms')->after('photo');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::table('agencies')->where('abbreviation', '=', 'HSA')->delete();
+		DB::table('agencies')->where('abbreviation', '=', 'RAID')->delete();
+		DB::table('agencies')->where('abbreviation', '=', 'other')->delete();
+
+        Schema::table('companies', function($table){
+		    $table->dropColumn('terms');
+		});
+	}
+
+}
