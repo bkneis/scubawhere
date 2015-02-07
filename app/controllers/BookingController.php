bkneis/scubawhere

commit 3dff63355b20f0d8421b35ff2f119790d50326af

Disable adding or removing of bookingdetails after the trip has departed
Resolves #69.

app/controllers/BookingController.php
@@ -310,6 +310,12 @@ class BookingController extends Controller {
 		}
 		*/
 
+		// Validate that the session start date has not already passed
+		if(Helper::isPast($departure->start))
+		{
+			return Response::json( array('errors' => array('Cannot add details, because the trip has already departed!')), 403 ); // 403 Forbidden
+		}
+
 		// Validate that the customer is not already booked for this session on another booking
 		$check = Auth::user()->bookings()
 			->whereNotIn('id', array($booking->id))
@@ -515,6 +521,12 @@ class BookingController extends Controller {
 		catch(ModelNotFoundException $e)
 		{
 			return Response::json( array('errors' => array('The bookingdetail has not been found.')), 404 ); // 404 Not Found
+		}
+
+		// Validate that the session start date has not already passed
+		if(Helper::isPast($departure->start))
+		{
+			return Response::json( array('errors' => array('Cannot remove details, because the trip has already departed!')), 403 ); // 403 Forbidden
 		}
 
 		// Execute delete

commit 39f55e39150b832b3e63b1051f93053ccbb198f5

Fix variable error in removeDetail introduced in 3dff633

app/controllers/BookingController.php
@@ -516,7 +516,7 @@ class BookingController extends Controller {
 		try
 		{
 			if( !Input::get('bookingdetail_id') ) throw new ModelNotFoundException();
-			$bookingdetail = $booking->bookingdetails()->findOrFail( Input::get('bookingdetail_id') );
+			$bookingdetail = $booking->bookingdetails()->with('departure')->findOrFail( Input::get('bookingdetail_id') );
 		}
 		catch(ModelNotFoundException $e)
 		{
@@ -524,7 +524,7 @@ class BookingController extends Controller {
 		}
 
 		// Validate that the session start date has not already passed
-		if(Helper::isPast($departure->start))
+		if(Helper::isPast($bookingdetail->departure->start))
 		{
 			return Response::json( array('errors' => array('Cannot remove details, because the trip has already departed!')), 403 ); // 403 Forbidden
 		}