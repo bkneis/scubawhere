f853dcdd45613b4e220a843f1d93240faf2cbc09
Soren Schwert <soren@scubawhere.com>
2015-02-15 17:31:45 +0000

    Controlled vendor update

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit4950ef88acddf5fc471389a645e83ea0::getLoader();
+return ComposerAutoloaderInit535eeaa747052f303d14244b3e81738c::getLoader();
