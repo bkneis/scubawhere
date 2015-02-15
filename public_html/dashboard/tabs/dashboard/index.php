7176261924067cbf32eacfa529885cf0b13ba0b9
Soren Schwert <soren@scubawhere.com>
2015-02-15 16:41:24 +0000

    Fix HTML on Dashboard and bootstrap row styling

--- public_html/dashboard/tabs/dashboard/index.php
+++ public_html/dashboard/tabs/dashboard/index.php
@@ -1,84 +1,83 @@
 <div id="wrapper" class="clearfix">
-  <div class="col-md-7">
 <div class="row" id="row1">
 
   <script type="text/x-handlebars-template" id="todays-sessions-widget">
-      <div class="col-md-7">
-    <div class="panel panel-default" id="todays-sessions">
-      <div class="panel-heading">
-        <h4 class="panel-title">Todays Trips</h4>
-      </div>
-      <div style="min-height:250px;" class="panel-body">
-        <table class="bluethead">
-          <thead>
-            <tr class="bg-primary">
-              <th>Trip</th>
-              <th>Boat</th>
-              <th>Capacity</th>
-              <th>Time</th>
-            </tr>
-          </thead>
-          <tbody id="sessions-list">
+    <div class="col-md-7">
+      <div class="panel panel-default" id="todays-sessions">
+        <div class="panel-heading">
+          <h4 class="panel-title">Todays Trips</h4>
+        </div>
+        <div style="min-height:250px;" class="panel-body">
+          <table class="bluethead">
+            <thead>
+              <tr class="bg-primary">
+                <th>Trip</th>
+                <th>Boat</th>
+                <th>Capacity</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody id="sessions-list">
 
-          </tbody>
-        </table>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
-  </div>
   </script>
 
   <script type="text/x-handlebars-template" id="setup-wizard">
-      <div class="col-md-7">
-    <div class="panel panel-default" id="setup-wizard">
-      <div class="panel-heading">
-        <h4 class="panel-title">Setup Wizard</h4>
-      </div>
-      <div style="min-height:250px;" class="panel-body">
-        <p>Welcome to scub rms ...</p>
-          <button id="start-wizard" class="btn btn-success text-uppercase">Start wizard</button>
+    <div class="col-md-7">
+      <div class="panel panel-default" id="setup-wizard">
+        <div class="panel-heading">
+          <h4 class="panel-title">Setup Wizard</h4>
+        </div>
+        <div style="min-height:250px;" class="panel-body">
+          <p>Welcome to scub rms ...</p>
+            <button id="start-wizard" class="btn btn-success text-uppercase">Start wizard</button>
+        </div>
       </div>
     </div>
-  </div>
   </script>
 
   <script type="text/x-handlebars-template" id="today-session-template">
     {{#each sessions}}
-    <tr class="accordion-header" data-id="{{id}}" id="today-session-{{id}}">
-      <td>{{trip.name}}</td>
-      <td>{{boat.name}}</td>
-      <td>{{getPer capacity}}</td>
-      <td>{{getTime start}} - {{getEnd start trip.duration}}</td>
-    </tr>
-    <tr class="accordion-body accordion-{{id}}">
-      <td colspan="9" style="overflow: auto;">
-        <table id="customers-{{id}}" class="table table-striped table-bordered cust-tbl" cellspacing="0" width="100%">
-          <thead>
-            <tr>
-              <th style="color:#313131">Name</th>
-              <th style="color:#313131">Email</th>
-              <th style="color:#313131">Country</th>
-              <th style="color:#313131">Phone Number</th>
-            </tr>
-          </thead>
-          <tbody id="customer-table-{{id}}">
-          </tbody>
-        </table>
-      </td>
-    </tr>
+      <tr class="accordion-header" data-id="{{id}}" id="today-session-{{id}}">
+        <td>{{trip.name}}</td>
+        <td>{{boat.name}}</td>
+        <td>{{getPer capacity}}</td>
+        <td>{{getTime start}} - {{getEnd start trip.duration}}</td>
+      </tr>
+      <tr class="accordion-body accordion-{{id}}">
+        <td colspan="9" style="overflow: auto;">
+          <table id="customers-{{id}}" class="table table-striped table-bordered cust-tbl" cellspacing="0" width="100%">
+            <thead>
+              <tr>
+                <th style="color:#313131">Name</th>
+                <th style="color:#313131">Email</th>
+                <th style="color:#313131">Country</th>
+                <th style="color:#313131">Phone Number</th>
+              </tr>
+            </thead>
+            <tbody id="customer-table-{{id}}">
+            </tbody>
+          </table>
+        </td>
+      </tr>
     {{else}}
-    <tr><td colspan="7" style="text-align: center;">You have no sessions today.</td></tr>
+      <tr><td colspan="7" style="text-align: center;">You have no sessions today.</td></tr>
     {{/each}}
   </script>
 
   <script type="text/x-handlebars-template" id="customer-details-template">
-      {{#each customers}}
+    {{#each customers}}
       <tr>
         <td>{{firstname}} {{lastname}}</td>
         <td>{{email}}</td>
         <td>{{country}}</td>
         <td>{{phone}}</td>
       </tr>
-      {{/each}}
+    {{/each}}
   </script>
 
   <div class="col-md-5">
@@ -90,6 +89,9 @@
       </div>
     </div>
   </div>
+
+</div><!-- .row -->
+<div class="row" id="row2">
 
   <div class="col-md-5">
     <div class="panel panel-default" id="recent-bookings">
@@ -115,103 +117,102 @@
       </div>
     </div>
   </div>
-  </div> 
 
   <script type="text/x-handlebars-template" id="tour-nav-wizard">
-  <div class="col-md-10">
-    <ul class="nav nav-wizard" role="tablist">
-      <li role="presentation" class="active">
-        <a href="javascript:void(0)" id="prog-acom" role="tab" class="selected" data-toggle="tab" data-target="#source-tab">
-          <span class="step-description">Accommodations</span>
-        </a>
-      </li>
-      <li role="presentation">
-        <a href="javascript:void(0)" role="tab" id="prog-agents" data-toggle="tab" data-target="#ticket-tab">
-          <span class="step-description">Agents</span>
-        </a>
-      </li>
-      <li role="presentation">
-        <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#customer-tab">
-          <span class="step-description">Locations</span>
-        </a>
-      </li>
-      <li role="presentation">
-        <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#session-tab">
-          <span class="step-description">Boats</span>
-        </a>
-      </li>
-      <li role="presentation">
-        <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#addon-tab">
-          <span class="step-description">Trips</span>
-        </a>
-      </li>
-      <li role="presentation">
-        <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#accommodation-tab">
-          <span class="step-description">Tickets</span>
-        </a>
-      </li>
-      <li role="presentation">
-        <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#extra-tab">
-          <span class="step-description">Packages</span>
-        </a>
-      </li>
-      <li role="presentation">
-        <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#summary-tab">
-          <span class="step-description">Add ons</span>
-        </a>
-      </li>
-    </ul>
-    </div>
-    <div class="col-md-2" style="min-height:80px" id="tour-button">
-      <button id="tour-next-step" class="btn btn-success text-uppercase">Next Step</button>
-    </div>
-    </script> 
+    <div class="col-md-10">
+      <ul class="nav nav-wizard" role="tablist">
+        <li role="presentation" class="active">
+          <a href="javascript:void(0)" id="prog-acom" role="tab" class="selected" data-toggle="tab" data-target="#source-tab">
+            <span class="step-description">Accommodations</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a href="javascript:void(0)" role="tab" id="prog-agents" data-toggle="tab" data-target="#ticket-tab">
+            <span class="step-description">Agents</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#customer-tab">
+            <span class="step-description">Locations</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#session-tab">
+            <span class="step-description">Boats</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#addon-tab">
+            <span class="step-description">Trips</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#accommodation-tab">
+            <span class="step-description">Tickets</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#extra-tab">
+            <span class="step-description">Packages</span>
+          </a>
+        </li>
+        <li role="presentation">
+          <a href="javascript:void(0)" role="tab" data-toggle="tab" data-target="#summary-tab">
+            <span class="step-description">Add ons</span>
+          </a>
+        </li>
+      </ul>
+      </div>
+      <div class="col-md-2" style="min-height:80px" id="tour-button">
+        <button id="tour-next-step" class="btn btn-success text-uppercase">Next Step</button>
+      </div>
+    </script>
 
   <script type="text/x-handlebars-template" id="booking-list-item-template">
     {{#each bookings}}
-    <tr class="accordion-header" data-id={{id}}>
+      <tr class="accordion-header" data-id={{id}}>
 
-      <td>{{reference}}</td>
-      <td>{{{lead_customer.firstname}}} {{{lead_customer.lastname}}}</td>
-      <td>{{lead_customer.phone}}</td>
-      <td>{{lead_customer.country.abbreviation}}</td>
-      <td>{{currency}} {{decimal_price}}</td>
-    </tr>
-    <tr class="accordion-body accordion-{{id}}">
-      <td colspan="5" style="overflow: auto;">
-        <div style="float: left; width: 180px; margin-right: 10px; border-right: 1px solid #C3D9F4;">
-          {{#if payments}}
-          <h4 class="text-center">Recieved Transactions</h4>
-          <table style="width: 160px;" class="table">
-            <tr>
-              <th>Date</th>
-              <th>Amount</th>
-              <th>Via</th>
-            </tr>
-            {{#each payments}}
-            <tr>
-              <td>{{received_at}}</td>
-              <td>{{currency}} {{amount}}</td>
-              <td>{{paymentgateway.name}}</td>
-            </tr>
-            {{/each}}
-            <tr>
-              <td></td>
-              <td class="table-sum">{{currency}} {{sumPaid}}</td>
-              <td>{{remainingPay}}</td>
-            </tr>
-          </table>
-          {{else}}
-          <h5 class="text-center text-muted">No transactions yet</h5>
-          {{/if}}
-        </div>
-        {{addTransactionButton}}
-        {{editButton}}
-      </td>
-    </tr>
-    <tr class="accordion-spacer accordion-{{id}}"></tr>
+        <td>{{reference}}</td>
+        <td>{{{lead_customer.firstname}}} {{{lead_customer.lastname}}}</td>
+        <td>{{lead_customer.phone}}</td>
+        <td>{{lead_customer.country.abbreviation}}</td>
+        <td>{{currency}} {{decimal_price}}</td>
+      </tr>
+      <tr class="accordion-body accordion-{{id}}">
+        <td colspan="5" style="overflow: auto;">
+          <div style="float: left; width: 180px; margin-right: 10px; border-right: 1px solid #C3D9F4;">
+            {{#if payments}}
+            <h4 class="text-center">Recieved Transactions</h4>
+            <table style="width: 160px;" class="table">
+              <tr>
+                <th>Date</th>
+                <th>Amount</th>
+                <th>Via</th>
+              </tr>
+              {{#each payments}}
+              <tr>
+                <td>{{received_at}}</td>
+                <td>{{currency}} {{amount}}</td>
+                <td>{{paymentgateway.name}}</td>
+              </tr>
+              {{/each}}
+              <tr>
+                <td></td>
+                <td class="table-sum">{{currency}} {{sumPaid}}</td>
+                <td>{{remainingPay}}</td>
+              </tr>
+            </table>
+            {{else}}
+            <h5 class="text-center text-muted">No transactions yet</h5>
+            {{/if}}
+          </div>
+          {{addTransactionButton}}
+          {{editButton}}
+        </td>
+      </tr>
+      <tr class="accordion-spacer accordion-{{id}}"></tr>
     {{else}}
-    <tr><td colspan="7" style="text-align: center;">You have no bookings yet.</td></tr>
+      <tr><td colspan="7" style="text-align: center;">You have no bookings yet.</td></tr>
     {{/each}}
   </script>
 
@@ -244,6 +245,8 @@
       </div>
     </div>
   </div>
+
+</div><!-- .row -->
 </div><!-- #wrapper -->
 
 <link rel="stylesheet" type="text/css" href="/common/css/datatables.css">
