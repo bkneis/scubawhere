6d0021612e4e144a67cb964ab427075664c5bb62
Soren Schwert <soren@scubawhere.com>
2015-02-19 22:58:25 +0000

    Access-Control-Allow-Methods
    [skip ci]

--- app/filters.php
+++ app/filters.php
@@ -22,6 +22,8 @@ App::before(function($request)
 	header('Access-Control-Allow-Origin: http://rms-test.scubawhere.com');
 
 	header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
+	header('Access-Control-Allow-Methods: GET, POST');
 });
 
 
