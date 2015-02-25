3500d3e41e269b23cbf8f5dbe5c7d7726fd99e43
Soren Schwert <soren@scubawhere.com>
2015-02-25 08:43:39 +0000

    Change to Krystal deploy script

--- deploy.krystal.sh
+++ deploy.krystal.sh
@@ -9,5 +9,8 @@ git pull
 # Pull updates for ezmonitor
 git --git-dir=/home/scubawhe/workbench/sisou/ezmonitor/.git --work-tree=/home/scubawhe/workbench/sisou/ezmonitor pull
 
+# Kill the ssh-agent
+ssh-agent -k
+
 php -d disable_functions= artisan migrate --force
 php -d disable_functions= artisan optimize
