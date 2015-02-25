4f06ba9eaf4fe84548a617e13ac2bfee077f112c
Soren Schwert <soren@scubawhere.com>
2015-02-25 02:26:22 +0000

    Fix Krystal deploy script
    This is the last change to the deploy scripts, i swear! :P
    
    [skip ci]

--- deploy.krystal.sh
+++ deploy.krystal.sh
@@ -1,16 +1,13 @@
 set -o errexit
 
 # Manually start ssh-agent and add deployment key to it
-ssh-agent /bin/bash
+eval `ssh-agent -s`
 ssh-add ~/.ssh/id_bitbucket_krystal
 
 git pull
 
 # Pull updates for ezmonitor
-# git --git-dir=/home/scubawhe/workbench/sisou/ezmonitor/.git --work-tree=/home/scubawhe/workbench/sisou/ezmonitor pull
-
-# Exit the bash that has been started with the ssh-agent
-exit
+git --git-dir=/home/scubawhe/workbench/sisou/ezmonitor/.git --work-tree=/home/scubawhe/workbench/sisou/ezmonitor pull
 
 php -d disable_functions= artisan migrate --force
 php -d disable_functions= artisan optimize
