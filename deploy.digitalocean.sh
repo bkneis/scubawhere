4bf709e5e7f3cb81ea77395416e570014b704568
Soren Schwert <soren@scubawhere.com>
2015-02-27 18:00:28 +0000

    Disable EZMonitor updates for now, as they seem to break the deployment
    [skip ci]

--- deploy.digitalocean.sh
+++ deploy.digitalocean.sh
@@ -3,7 +3,7 @@ set -o errexit
 git pull
 
 # Pull updates for ezmonitor
-git --git-dir=/home/scubawhere/applications/rms/workbench/sisou/ezmonitor/.git --work-tree=/home/scubawhere/applications/rms/workbench/sisou/ezmonitor pull
+# git --git-dir=/home/scubawhere/applications/rms/workbench/sisou/ezmonitor/.git --work-tree=/home/scubawhere/applications/rms/workbench/sisou/ezmonitor pull
 
 php artisan migrate --force
 php artisan optimize
