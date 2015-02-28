1b5d053f32edeeb8789f2042f66bbab1a825406a
Jonathan Lambert <jonathan@scubawhere.com>
2015-02-28 14:51:48 +0000

    Updated gruntfile with concat and uglify settings

--- public_html/Gruntfile.js
+++ public_html/Gruntfile.js
@@ -5,6 +5,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     
     grunt.initConfig({
         watch: {
@@ -36,8 +38,32 @@ module.exports = function (grunt) {
                     'dashboard/common/css/bootstrap-scubawhere.min.css': ['dashboard/common/css/bootstrap-scubawhere.css']
                 }
             }
+        },
+        concat: {
+            basic: {
+                src: [
+                    'dashboard/js/Controllers/*.js',
+                    'dashboard/js/main.js',
+                    'dashboard/js/ui.js',
+                    'dashboard/js/navigation.js',
+                    'dashboard/js/validate.js',
+                    'dashboard/js/tour.js',
+
+                ],
+                dest: 'dashboard/js/scubawhere.js'
+            }
+        },
+        uglify: {
+            basic: {
+                files: {
+                    'dashboard/js/scubawhere.min.js': ['dashboard/js/scubawhere.js']
+                }
+            }
         }
     });
 
     grunt.registerTask('default', ['watch']);
+    
+    grunt.registerTask('dev', ['concat']);
+    grunt.registerTask('production', ['concat', 'uglify']);
 };
