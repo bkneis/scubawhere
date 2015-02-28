1cf8a7005dc4d2baeae6278e4286c3dfc3d03610
Jonathan Lambert <jonathan@scubawhere.com>
2015-02-28 12:13:33 +0000

    Added cssmin module to Gruntfile.js

--- public_html/Gruntfile.js
+++ public_html/Gruntfile.js
@@ -4,6 +4,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     
     grunt.initConfig({
         watch: {
@@ -28,6 +29,13 @@ module.exports = function (grunt) {
         },
         jshint: {
             files: ['Gruntfile.js', 'dashboard/js/**/*.js', 'dashboard/tabs/**/*.js']
+        },
+        cssmin: {
+            target: {
+                files: {
+                    'dashboard/common/css/bootstrap-scubawhere.min.css': ['dashboard/common/css/bootstrap-scubawhere.css']
+                }
+            }
         }
     });
 
