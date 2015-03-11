d9536c89a1b00c745abc357cd1948396e0008a3b
bkneis <bryan@iqwebcreations.com>
2015-03-11 16:03:49 +0000

    Reports updated
    Refunds added, summary fixed and dates now automatically get current
    week
    --skip-ci

--- public_html/dashboard/tabs/reports/index.php
+++ public_html/dashboard/tabs/reports/index.php
@@ -2,7 +2,7 @@
 	<div class="col-md-12">
 		<div class="panel panel-default">
 			<div class="panel-heading">
-				<h4 class="panel-title">Transactions Report</h4>
+				<h4 id="report-title" class="panel-title">Transactions Report</h4>
 			</div>
 			<div class="panel-body">
 				<div id="report-type-btns" class="btn-group col-md-offset-3" role="group">
@@ -16,21 +16,21 @@
 				<div style="margin-top:20px"></div>
 
 				<div class="row row-list">
-				    <div class="col-xs-3">
-				    	<div class="input-group">
+					<div class="col-xs-3">
+						<div class="input-group">
 							<label class="input-group-addon">From : </label>
 							<input style="width:200px; float:left;" id="start-date" type="text" class="form-control datepicker" data-date-format="YYYY-MM-DD" name="jumpto" placeholder="YYYY-MM-DD">
 						</div>
-				    </div>
-				    <div class="col-xs-3">
-				    	<div class="input-group">
+					</div>
+					<div class="col-xs-3">
+						<div class="input-group">
 							<label class="input-group-addon">Until : </label>
 							<input style="width:200px; float:left;" id="end-date" type="text" class="form-control datepicker" data-date-format="YYYY-MM-DD" name="jumpto" placeholder="YYYY-MM-DD">
 						</div>
-				    </div>
-				    <div class="col-xs-6">
-				    	<div id="report-filters" class="pull-right"></div>
-				    </div>
+					</div>
+					<div class="col-xs-6">
+						<div id="report-filters" class="pull-right"></div>
+					</div>
 				</div> 
 
 				<div style="margin-top:20px"></div>
@@ -43,151 +43,169 @@
 <script type="text/x-handlebars-template" id="transactions-report-template">
 	<table class="table table-striped table-bordered reports-table" cellspacing="0" width="100%">
 		<thead>
-  			<tr>
-  				<th style="color:#313131; width:25%">Date</th>
-                <th style="color:#313131; width:33%">Name</th>
-                <th style="color:#313131">Type</th>
-                <th style="color:#313131">Amount</th>
-            </tr>
+			<tr>
+				<th style="color:#313131; width:25%">Date</th>
+				<th style="color:#313131; width:33%">Name</th>
+				<th style="color:#313131">Type</th>
+				<th style="color:#313131">Amount</th>
+			</tr>
 		</thead>
 		<tbody>
 			{{#each entries}}
-				<tr>
-					<td>{{received_at}}</td>
-					<td>{{booking.lead_customer.firstname}} {{booking.lead_customer.lastname}}</td>
-					<td>{{paymentgateway.name}}</td>
-					<td>{{currency.symbol}} {{amount}}</td>
-				</tr>
+			{{#if refund}}
+			<tr style="color:#F00">
+				<td>{{received_at}}</td>
+				<td>{{{booking.lead_customer.firstname}}} {{{booking.lead_customer.lastname}}}</td>
+				<td>{{paymentgateway.name}} REFUND</td>
+				<td>- {{currency.symbol}} {{amount}}</td>
+			</tr>
 			{{else}}
-				<tr><td colspan="4" class="text-center">There are no transactions between these dates</td></tr>
+			<tr>
+				<td>{{received_at}}</td>
+				<td>{{{booking.lead_customer.firstname}}} {{{booking.lead_customer.lastname}}}</td>
+				<td>{{paymentgateway.name}}</td>
+				<td>{{currency.symbol}} {{amount}}</td>
+			</tr>
+			{{/if}}
+			{{else}}
+			<tr><td colspan="4" class="text-center">There are no transactions between these dates</td></tr>
 			{{/each}}
 		</tbody>
-		<table>
-			<thead style="font-weight: bold;">
-				<tr>
-					<td></td>
-					<td></td>
-					<td class="pull-right" id="transactions-date-range">Total </td>
-					<td id="transactions-cash">
-						Cash
-						<div style="width:100%" class="percentage-bar-container bg-success border-success">	
+		
+	</table>
+	<table>
+		<thead style="font-weight: bold;">
+			<tr>
+				<td></td>
+				<td></td>
+				<td class="pull-right" id="transactions-date-range">Total </td>
+				<td id="transactions-cash">
+					Cash
+					<div style="width:100%" class="percentage-bar-container bg-success border-success">	
 						<div id="transactions-cash-percentage" class="percentage-bar" style="background-color: #5cb85c;">&nbsp;</div>	
 						<span id="transactions-totalCash" class="percentage-left"></span>
-						</div>
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td id="transactions-credit">
-						Credit card
-						<div style="width:100%" class="percentage-bar-container bg-success border-success">	
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td id="transactions-credit">
+					Credit card
+					<div style="width:100%" class="percentage-bar-container bg-success border-success">	
 						<div id="transactions-credit-percentage" class="percentage-bar" style="background-color: #5cb85c;">&nbsp;</div>	
 						<span id="transactions-totalCredit" class="percentage-left"></span>
-						</div>
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td id="transactions-cheque">
-						Cheques
-						<div style="width:100%" class="percentage-bar-container bg-success border-success">	
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td id="transactions-cheque">
+					Cheques
+					<div style="width:100%" class="percentage-bar-container bg-success border-success">	
 						<div id="transactions-cheque-percentage" class="percentage-bar" style="background-color: #5cb85c;">&nbsp;</div>	
 						<span id="transactions-totalCheque" class="percentage-left"></span>
-						</div>
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td id="transactions-bank">
-						Bank
-						<div style="width:100%" class="percentage-bar-container bg-success border-success">	
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td id="transactions-bank">
+					Bank
+					<div style="width:100%" class="percentage-bar-container bg-success border-success">	
 						<div id="transactions-bank-percentage" class="percentage-bar" style="background-color: #5cb85c;">&nbsp;</div>	
 						<span id="transactions-totalBank" class="percentage-left"></span>
-						</div>
-					</td>
-				</tr>
-				<tr>
-					<td></td>
-					<td></td>
-					<td></td>
-					<td id="transactions-paypal">
-						Credit card
-						<div style="width:100%" class="percentage-bar-container bg-success border-success">	
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td id="transactions-paypal">
+					Paypal
+					<div style="width:100%" class="percentage-bar-container bg-success border-success">	
 						<div id="transactions-paypal-percentage" class="percentage-bar" style="background-color: #5cb85c;">&nbsp;</div>	
 						<span id="transactions-totalPaypal" class="percentage-left"></span>
-						</div>
-					</td>
-				</tr>
-			</thead>
-		</table>
+					</div>
+				</td>
+			</tr>
+		</thead>
 	</table>
 </script>
 <script type="text/x-handlebars-template" id="agents-report-template">
 	<table class="table table-striped table-bordered reports-table" cellspacing="0" width="100%">
 		<thead>
-  			<tr>
-  				<th style="color:#313131">Date</th>
-                <th style="color:#313131">Name</th>
-                <th style="color:#313131">Agent name</th>
-                <th style="color:#313131">Total balance</th>
-                <th style="color:#313131">Commission</th>
-                <th style="color:#313131">Cleared balance</th>
-            </tr>
+			<tr>
+				<th style="color:#313131">Date</th>
+				<th style="color:#313131">Name</th>
+				<th style="color:#313131">Agent name</th>
+				<th style="color:#313131">Total balance</th>
+				<th style="color:#313131">Commission</th>
+				<th style="color:#313131">Cleared balance</th>
+			</tr>
 		</thead>
 		<tbody>
 			{{#each entries.bookings}}
-				<tr>
-					<td>{{arrival_date}}</td>
-					<td>{{lead_customer.firstname}} {{lead_customer.lastname}}</td>
-					<td>{{agent}}</td>
-					<td>{{decimal_price}}</td>
-					<td></td>
-					<td></td>
-				</tr>
-			{{else}}
-				<tr><td colspan="6" class="text-center">There are no agent bookings between these dates</td></tr>
-			{{/each}}
-		</tbody>
-		<tfoot style="font-weight: bold;">
 			<tr>
-				<td>Date range</td>
-				<td>Total</td>
-				<td>Agent name</td>
+				<td>{{arrival_date}}</td>
+				<td>{{{lead_customer.firstname}}} {{{lead_customer.lastname}}}</td>
+				<td>{{{agent.name}}}</td>
+				<td>{{currency}} {{decimal_price}}</td>
+				<td>{{agent.commission}}%</td>
 				<td></td>
 			</tr>
-		</tfoot>
+			{{else}}
+			<tr><td colspan="6" class="text-center">There are no agent bookings between these dates</td></tr>
+			{{/each}}
+		</tbody>
 	</table>
+	{{!--<table>
+		<thead style="font-weight: bold;">
+			<tr>
+				<td></td>
+				<td></td>
+				<td class="pull-right">Date range </td>
+				<td>
+					Total
+					<div style="width:100%" class="percentage-bar-container bg-success border-success">	
+					<div id="agents-percentage-total" class="percentage-bar" style="background-color: #5cb85c;">&nbsp;</div>	
+					<span id="agents-total" class="percentage-left"></span>
+					</div>
+				</td>
+			</tr>
+		</thead>
+	</table>--}}
 </script>
 <script type="text/x-handlebars-template" id="booking-history-report-template">
 	<table class="table table-striped table-bordered reports-table" cellspacing="0" width="100%">
 		<thead>
-  			<tr>
-  				<th style="color:#313131">Date</th>
-                <th style="color:#313131">Name</th>
-                <th style="color:#313131">Method of booking</th>
-                <th style="color:#313131">Country</th>
-                <th style="color:#313131">Refrence</th>
-                <th style="color:#313131">Total cost</th>
-            </tr>
+			<tr>
+				<th style="color:#313131">Date</th>
+				<th style="color:#313131">Name</th>
+				<th style="color:#313131">Method of booking</th>
+				<th style="color:#313131">Country</th>
+				<th style="color:#313131">Refrence</th>
+				<th style="color:#313131">Total cost</th>
+			</tr>
 		</thead>
 		<tbody>
 			{{#each entries.bookings}}
-				<tr>
-					<td>{{arrival_date}}</td>
-					<td>{{lead_customer.firstname}} {{lead_customer.lastname}}</td>
-					<td>{{source}}</td>
-					<td>{{lead_customer.country_id}}</td>
-					<td>{{reference}}</td>
-					<td>{{decimal_price}}</td>
-				</tr>
+			<tr>
+				<td>{{arrival_date}}</td>
+				<td>{{{lead_customer.firstname}}} {{{lead_customer.lastname}}}</td>
+				<td>{{#if source}} {{source}} {{else}} Agent - {{{agent.name}}} {{/if}}</td>
+				<td>{{getCountry lead_customer.country_id}}</td>
+				<td>{{reference}}</td>
+				<td>{{currency}} {{decimal_price}}</td>
+			</tr>
 			{{else}}
-				<tr><td colspan="6" class="text-center">There are no bookings between these dates</td></tr>
+			<tr><td colspan="6" class="text-center">There are no bookings between these dates</td></tr>
 			{{/each}}
 		</tbody>
 	</table>
@@ -195,27 +213,27 @@
 <script type="text/x-handlebars-template" id="utilisation-report-template">
 	<table class="table table-striped table-bordered reports-table" cellspacing="0" width="100%">
 		<thead>
-  			<tr>
-                <th style="color:#313131; width:15%">Date</th>
-                <th style="color:#313131; width:15%">Trip name</th>
-                <th style="color:#313131">Utilisation</th>
-            </tr>
+			<tr>
+				<th style="color:#313131; width:15%">Date</th>
+				<th style="color:#313131; width:15%">Trip name</th>
+				<th style="color:#313131">Utilisation</th>
+			</tr>
 		</thead>
 		<tbody>
 			{{#each entries.utilisation}}
-				<tr>
-					<td>{{getDate date}}</td>
-					<td>{{name}}</td>
-					<td>
-						
-						<div style="width:100%" class="percentage-bar-container bg-success border-success">	
-							<div class="percentage-bar" style="background-color: #5cb85c; width: {{getUtil capacity unassigned}}%;">{{getRemaining capacity unassigned}}</div>	
-							<span class="percentage-left">{{capacity}}</span>
-						</div>
-					</td>
-				</tr>
+			<tr>
+				<td>{{getDate date}}</td>
+				<td>{{{name}}}</td>
+				<td>
+
+					<div style="width:100%" class="percentage-bar-container bg-success border-success">	
+						<div class="percentage-bar" style="background-color: #5cb85c; width: {{getUtil capacity unassigned}}%;">{{getRemaining capacity unassigned}}</div>	
+						<span class="percentage-left">{{capacity}}</span>
+					</div>
+				</td>
+			</tr>
 			{{else}}
-				<tr><td colspan="3" class="text-center">There are no trips between these dates</td></tr>
+			<tr><td colspan="3" class="text-center">There are no trips between these dates</td></tr>
 			{{/each}}
 		</tbody>
 		<table>
@@ -228,55 +246,56 @@
 							<span id="utilisation-total-capacity" class="percentage-left"></span>
 						</div>
 					</td>
-			</thead>
+				</thead>
+			</table>
 		</table>
-	</table>
-</script>
-<script type="text/x-handlebars-template" id="agents-filter-template">
-	<div class="input-group">
-		<label class="input-group-addon">Filter by : </label>
-		<select>
-			<option>Please select ...</option>
-			{{#each agents}}
-				<option value="{{id}}">{{name}}</option>
-			{{/each}}
-		</select>
-	</div>
-</script>
-<script type="text/x-handlebars-template" id="transactions-filter-template">
-	<div class="input-group">
-		<label class="input-group-addon">Filter by : </label>
-		<select>
-			<option>Please select ...</option>
-			{{#each gateways}}
-				<option value="{{id}}">{{name}}</option>
-			{{/each}}
-		</select>
-	</div>
-</script>
-<script type="text/x-handlebars-template" id="booking-history-filter-template">
-	<div class="input-group">
-		<label class="input-group-addon">Filter by : </label>
-		<select>
-			<option>Please select ...</option>
-			{{#each sources}}
-				<option value="{{name}}">{{name}}</option>
-			{{/each}}
-		</select>
-	</div>
-</script>
-<script type="text/x-handlebars-template" id="utilisation-filter-template">
-	<div class="input-group">
-		<label class="input-group-addon">Filter by : </label>
-		<select>
-			<option>Please select ...</option>
-			{{#each trips}}
-				<option value="{{id}}">{{name}}</option>
-			{{/each}}
-		</select>
-	</div>
-</script>
-<script type="text/javascript" src="js/Controllers/Agent.js"></script>
-<script type="text/javascript" src="js/Controllers/Trip.js"></script>
-<script src="/common/js/jquery/jquery.datatables.min.js"></script>
-<script type="text/javascript" src="tabs/reports/js/script.js"></script>
+	</script>
+	<script type="text/x-handlebars-template" id="agents-filter-template">
+		<div class="input-group">
+			<label class="input-group-addon">Filter by : </label>
+			<select>
+				<option>Please select ...</option>
+				{{#each agents}}
+					<option value="{{id}}">{{{name}}}</option>
+				{{/each}}
+			</select>
+		</div>
+	</script>
+	<script type="text/x-handlebars-template" id="transactions-filter-template">
+		<div class="input-group">
+			<label class="input-group-addon">Filter by : </label>
+			<select>
+				<option>Please select ...</option>
+				{{#each gateways}}
+					<option value="{{id}}">{{{name}}}</option>
+				{{/each}}
+			</select>
+		</div>
+	</script>
+	<script type="text/x-handlebars-template" id="booking-history-filter-template">
+		<div class="input-group">
+			<label class="input-group-addon">Filter by : </label>
+			<select>
+				<option>Please select ...</option>
+				{{#each sources}}
+				<option value="{{name}}">{{{name}}}</option>
+				{{/each}}
+			</select>
+		</div>
+	</script>
+	<script type="text/x-handlebars-template" id="utilisation-filter-template">
+		<div class="input-group">
+			<label class="input-group-addon">Filter by : </label>
+			<select id="utilisation-filter">
+				<option>Please select ...</option>
+				{{#each trips}}
+					<option value="{{id}}">{{{name}}}</option>
+				{{/each}}
+			</select>
+		</div>
+	</script>
+	<script type="text/javascript" src="js/Controllers/Agent.js"></script>
+	<script type="text/javascript" src="js/Controllers/Trip.js"></script>
+	<script type="text/javascript" src="js/Controllers/Report.js"></script>
+	<script src="/common/js/jquery/jquery.datatables.min.js"></script>
+	<script type="text/javascript" src="tabs/reports/js/script.js"></script>
--- public_html/dashboard/tabs/reports/js/script.js
+++ public_html/dashboard/tabs/reports/js/script.js
@@ -1,6 +1,7 @@
 var report_type;
 var report;
 var filter;
+var summaries;
 
 Handlebars.registerHelper('getUtil', function(capacity, unassigned){
 	return Math.round((100-((unassigned/capacity) * 100)));
@@ -18,7 +19,22 @@ Handlebars.registerHelper('getRemaining', function(capacity, unassigned){
 	return capacity - unassigned;
 });
 
+Handlebars.registerHelper('getCountry', function(id){
+	return _.findWhere(window.countries, {id: id}).name;
+});
+
+Handlebars.registerHelper('currency', function() {
+	return window.company.currency.symbol;
+});
+
 $(function() {
+
+	$.ajax({
+		url : "/api/country/all",
+		success : function(data) {
+			window.countries = _.indexBy(data, "id");
+		}
+	});
 
 	report_type = "transactions";
 
@@ -34,10 +50,10 @@ $(function() {
 		autoclose : true
 	});
 
-	$('#start-date').val('2015-01-01');;
-	$('#end-date').val('2015-05-01');
+	$('#start-date').val(getDates().lastWeek);
+	$('#end-date').val(getDates().todayDate);
 
-	$('.reports-table').DataTable({
+	var dataTable = $('.reports-table').DataTable({
 		"paging":   false,
 		"ordering": false,
 		"info":     false,
@@ -62,20 +78,39 @@ $(function() {
 });
 
 function getDates() {
-	var dates = {};
 	var today = new Date();
 	var dd = today.getDate();
 	var mm = today.getMonth()+1; //January is 0!
 	var yyyy = today.getFullYear();
 
-	if(dd<10) dd='0'+dd;
-	if(mm<10) mm='0'+mm;
-	//dates.start = (today.getDate() - 7);
-	today = yyyy+'-'+mm+'-'+dd;
-	dates.end = yyyy+'-'+mm+'-'+dd;
-	dates.start = (today.getDate() - 7);
-	console.log(dates);
-	//return dates;
+	if(dd<10) {
+	    dd='0'+dd
+	} 
+
+	if(mm<10) {
+	    mm='0'+mm
+	} 
+
+	var lastWeek = new Date();
+	lastWeek.setDate(today.getDate() - 7);
+	var dd2 = lastWeek.getDate();
+	var mm2 = lastWeek.getMonth()+1; //January is 0!
+	var yyyy2 = lastWeek.getFullYear();
+
+	if(dd2<10) {
+	    dd2='0'+dd2
+	} 
+
+	if(mm2<10) {
+	    mm2='0'+mm2
+	}
+
+	var dates = {
+		todayDate : yyyy+'-'+mm+'-'+dd,
+		lastWeek : yyyy2+'-'+mm2+'-'+dd2
+	};
+
+	return dates;
 }
 
 function getReport(reportType) {
@@ -86,104 +121,113 @@ function getReport(reportType) {
 	$("#report-filters").empty();
 	switch(reportType) {
 		case("transactions") :
+			$("#report-title").empty().append("Transactions Report");
 			filter = Handlebars.compile($("#transactions-filter-template").html());
-			$.ajax({
-				url: '/api/payment/paymentgateways',
-				success: function(data) {
-					$("#report-filters").empty().append( filter({gateways : data}) );
-				}
-			});
-			$.ajax({
-			url: '/api/payment/filter',
-			data: dates,
-			success: function(data) {
-				console.log(data);
-				report = Handlebars.compile($("#transactions-report-template").html());
-				$("#reports").empty().append( report({entries : data}) );
-				var totalCash = 0, totalCredit = 0, totalCheque = 0, totalBank = 0, totalPaypal = 0;
-				for(var i=0; i < data.length; i++) {
-					console.log(data[i].amount);
-					console.log(data[i].paymentgateway_id);
-					switch(data[i].paymentgateway_id) {
-						case("1") :
-							totalCash += parseInt(data[i].amount);
-							break;
-						case("2") :
-							totalCredit += parseInt(data[i].amount);
-							break;
-						case("3") :
-							totalCheque += parseInt(data[i].amount);
-							break;
-						case("4") :
-							totalBank += parseInt(data[i].amount);
-							break;
-						case("5") :
-							totalPaypal += parseInt(data[i].amount);
-							break;
+			report = Handlebars.compile($("#transactions-report-template").html());
+			//summaries = Handlebars.compile($("#transactions-totals-template").html());
+			
+			Report.getPayments(dates, function success(data) {
+				Report.getRefunds(dates, function success(data2) {
+					var newData = data.concat(data2);
+					for(var i = 0; i < data2.length; i++) {
+						data2[i].refund = true;
 					}
-				}
-				var total = totalCash + totalCredit + totalCheque + totalBank + totalPaypal;
-				$("#transactions-totalCash").text(data[0].currency.symbol + " " + totalCash);
-				$("#transactions-cash-percentage").css("width", ((totalCash/total)*100) + "%");
-				$("#transactions-totalCredit").text(data[0].currency.symbol + " " + totalCredit);
-				$("#transactions-credit-percentage").css("width", ((totalCredit/total)*100) + "%");
-				$("#transactions-totalCheque").text(data[0].currency.symbol + " " + totalCheque);
-				$("#transactions-cheque-percentage").css("width", ((totalCheque/total)*100) + "%");
-				$("#transactions-totalBank").text(data[0].currency.symbol + " " + totalBank);
-				$("#transactions-bank-percentage").css("width", ((totalBank/total)*100) + "%");
-				$("#transactions-totalPaypal").text(data[0].currency.symbol + " " + totalPaypal);
-				$("#transactions-paypal-percentage").css("width", ((totalPaypal/total)*100) + "%");
-				$("#transactions-date-range").append(" from " + $("#start-date").val() + " until " + $("#end-date").val());
-			}
-		});
+					console.log(newData);
+					$("#reports").empty().append( report({entries : newData}) );
+					var totalCash = 0, totalCredit = 0, totalCheque = 0, totalBank = 0, totalPaypal = 0;
+					for(var i=0; i < newData.length; i++) {
+						switch(parseInt(newData[i].paymentgateway_id)) {
+							case(1) :
+								if(newData[i].refund) totalCash -= parseInt(newData[i].amount);
+								else totalCash += parseInt(newData[i].amount);
+								break;
+							case(2) :
+								if(newData[i].refund) totalCredit -= parseInt(newData[i].amount);
+								else totalCredit += parseInt(newData[i].amount);
+								break;
+							case(3) :
+								if(newData[i].refund) totalCheque -= parseInt(newData[i].amount);
+								else totalCheque += parseInt(newData[i].amount);
+								break;
+							case(4) :
+								if(newData[i].refund) totalBank -= parseInt(newData[i].amount);
+								else totalBank += parseInt(newData[i].amount);
+								break;
+							case(5) :
+								if(newData[i].refund) totalPaypal -= parseInt(newData[i].amount);
+								else totalPaypal += parseInt(newData[i].amount);
+								break;
+						}
+					}
+					var total = totalCash + totalCredit + totalCheque + totalBank + totalPaypal;
+					$("#transactions-totalCash").text(data[0].currency.symbol + " " + totalCash);
+					$("#transactions-cash-percentage").css("width", ((totalCash/total)*100) + "%");
+					$("#transactions-totalCredit").text(data[0].currency.symbol + " " + totalCredit);
+					$("#transactions-credit-percentage").css("width", ((totalCredit/total)*100) + "%");
+					$("#transactions-totalCheque").text(data[0].currency.symbol + " " + totalCheque);
+					$("#transactions-cheque-percentage").css("width", ((totalCheque/total)*100) + "%");
+					$("#transactions-totalBank").text(data[0].currency.symbol + " " + totalBank);
+					$("#transactions-bank-percentage").css("width", ((totalBank/total)*100) + "%");
+					$("#transactions-totalPaypal").text(data[0].currency.symbol + " " + totalPaypal);
+					$("#transactions-paypal-percentage").css("width", ((totalPaypal/total)*100) + "%");
+					$("#transactions-date-range").append(" from " + $("#start-date").val() + " until " + $("#end-date").val());
+				});
+			});
 			break;
+
 		case("agents") :
+			$("#report-title").empty().append("Agent Transactions Report");
 			filter = Handlebars.compile($("#agents-filter-template").html());
+
 			Agent.getAllAgents(function sucess(data) {
 				$("#report-filters").empty().append( filter({agents : data}) );
 			});
-			$.ajax({
-				url: '/api/booking/filter-confirmed-by-agent',
-				data: dates,
-				success: function(data) {
-					console.log(data);
-					report = Handlebars.compile($("#agents-report-template").html());
-					$("#reports").empty().append( report({entries : data}) );
-				}
+			
+			Report.getAgentBookings(dates, function success(data) {
+				console.log(data);
+				report = Handlebars.compile($("#agents-report-template").html());
+				$("#reports").empty().append( report({entries : data}) );
+				/*var agentsTotal = 0;
+					for(var i=0; i < data.bookings.length; i++) 
+					{
+						agentsTotal += parseInt(data.bookings[i].decimal_price);
+					}
+					$("#agents-total").text(agentsTotal);
+					$("#agents-percentage-total").css("width", agentsTotal + "%");*/
 			});
 			break;
+
 		case("booking-history") :
+			$("#report-title").empty().append("Booking History Report");
 			filter = Handlebars.compile($("#booking-history-filter-template").html());
+
 			var data = [{name : "Telephone"}, {name: "Agent"}, {name: "In person"}, {name: "Email"}];
 			$("#report-filters").empty().append( filter({sources : data}) );
-			$.ajax({
-				url: '/api/booking/filter-confirmed',
-				data: dates,
-				success: function(data) {
-					console.log(data);
-					report = Handlebars.compile($("#booking-history-report-template").html());
-					$("#reports").empty().append( report({entries : data}) );
-				}
+			
+			Report.getBookingHistory(dates, function success(data) {
+				console.log(data);
+				report = Handlebars.compile($("#booking-history-report-template").html());
+				$("#reports").empty().append( report({entries : data}) );
 			});
 			break;
+
 		case("utilisation") :
+			$("#report-title").empty().append("Trip Utilisation Report");
 			filter = Handlebars.compile($("#utilisation-filter-template").html());
+
 			Trip.getAllTrips(function sucess(data) {
 				$("#report-filters").empty().append( filter({trips : data}) );
 			});
-			$.ajax({
-				url: '/api/report/utilisation',
-				data: dates,
-				success: function(data) {
-					console.log(data);
-					report = Handlebars.compile($("#utilisation-report-template").html());
-					$("#reports").empty().append( report({entries : data}) );
-					$("#utilisation-total-capacity").text(data.utilisation_total.unassigned);
-					$("#utilisation-average").css("width", (100 - ((data.utilisation_total.unassigned/data.utilisation_total.capacity)*100)) + "%");
-					$("#utilisation-date-range").append(" from " + $("#start-date").val() + " until " + $("#end-date").val());
-				}
+
+			Report.getTripUtilisation(dates, function sucess(data) {
+				console.log(data);
+				report = Handlebars.compile($("#utilisation-report-template").html());
+				$("#reports").empty().append( report({entries : data}) );
+				$("#utilisation-total-capacity").text(data.utilisation_total.unassigned);
+				$("#utilisation-average").css("width", (100 - ((data.utilisation_total.unassigned/data.utilisation_total.capacity)*100)) + "%");
+				$("#utilisation-date-range").append(" from " + $("#start-date").val() + " until " + $("#end-date").val());
 			});
-			
+
 			break;
 	}
 }
