9222c88b74c45db78b31784acf6c10176ea35cbd
bkneis <bryan@iqwebcreations.com>
2015-03-11 16:00:46 +0000

    Added reports controller
    --skip-ci

---
+++ public_html/dashboard/js/Controllers/Report.js
@@ -0,0 +1,47 @@
+var Report = {
+	
+	getPaymentGateways : function(handleData) {
+		$.ajax({ url: '/api/payment/paymentgateways', success: handleData });
+	},
+
+	getPayments : function(params, handleData) {
+		$.ajax({
+			url: '/api/payment/filter',
+			data: params,
+			success: handleData
+		});
+	},
+
+	getRefunds : function(params, handleData) {
+		$.ajax({
+			url: '/api/refund/filter',
+			data: params,
+			success: handleData
+		});
+	},
+
+	getAgentBookings : function(params, handleData) {
+		$.ajax({
+			url: '/api/booking/filter-confirmed-by-agent',
+			data: params,
+			success: handleData
+		});
+	},
+
+	getBookingHistory : function(params, handleData) {
+		$.ajax({
+			url: '/api/booking/filter-confirmed',
+			data: params,
+			success: handleData
+		});
+	},
+
+	getTripUtilisation : function(params, handleData) {
+		$.ajax({
+			url: '/api/report/utilisation',
+			data: params,
+			success: handleData
+		});
+	}
+
+};
