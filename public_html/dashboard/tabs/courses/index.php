a4509a2e37405f96a2e58a60220b4792e869a08b
bkneis <bryan@iqwebcreations.com>
2015-03-21 18:32:58 +0000

    Courses now completed (except tour)

--- public_html/dashboard/tabs/courses/index.php
+++ public_html/dashboard/tabs/courses/index.php
@@ -1,18 +1,18 @@
 <div id="wrapper" class="clearfix">
-	<div id="tour-div" style="width:0px; height:0px; margin-left:50%;" data-step="1" data-intro="Now, we need to add your classes. A class can be valid for many trips. A class is a single reservation for a trip. For an educational course please create a package (see next page)."></div>
+	<div id="tour-div" style="width:0px; height:0px; margin-left:50%;" data-step="1" data-intro="Now you need to add your packages. A package consists of many tickets and is great for offering deals that include more than 1 ticket. A package is also used for educational courses that include multiple trips."></div>
 
 	<div class="row">
 		<div class="col-md-4">
-			<div id="classes-list-div" class="panel panel-default" data-step="7" data-position="right" data-intro="Once a class is saved, you will see it in your list. Click on a class to view/edit the details.">
+			<div class="panel panel-default" id="packages-list-div" data-step="7" data-position="right" data-intro="Once a package is saved, you will see it in your list. Click on a package to view/edit the details.">
 				<div class="panel-heading">
 					<h4 class="panel-title">Available Courses</h4>
 				</div>
 				<div class="panel-body" id="course-list-container">
-					<button id="change-to-add-course" class="btn btn-success text-uppercase">&plus; Add course</button>
+					<button id="change-to-add-course" class="btn btn-success text-uppercase">&plus; Add Course</button>
 					<script type="text/x-handlebars-template" id="course-list-template">
 						<ul id="course-list" class="entity-list">
 							{{#each courses}}
-								<li data-id="{{id}}"><strong>{{{name}}}</strong></li>
+								<li data-id="{{id}}"><strong>{{{name}}}</strong> | {{count tickets}} tickets</li>
 							{{else}}
 								<p id="no-courses">No courses available.</p>
 							{{/each}}
@@ -23,17 +23,20 @@
 		</div>
 
 		<div class="col-md-8">
-			<div class="panel panel-default" id="course-form-container" data-step="2" data-position="left" data-intro="Enter a name, description and base price for the class.">
+			<div class="panel panel-default" id="course-form-container" data-step="2" data-position="left" data-intro="Enter a name, description and price for the package.">
 				<script type="text/x-handlebars-template" id="course-form-template">
 					<div class="panel-heading">
 						<h4 class="panel-title">{{task}} Course</h4>
 					</div>
 					<div class="panel-body">
 						<form id="{{task}}-course-form">
-
 							<div class="form-row">
 								<label class="field-label">Course Name</label>
 								<input id="course-name" type="text" name="name" value="{{{name}}}">
+
+								{{#if update}}
+									<span class="btn btn-danger pull-right remove-course">Remove</span>
+								{{/if}}
 							</div>
 
 							<div class="form-row">
@@ -46,20 +49,55 @@
 								<input id="course-capacity" type="number" name="capacity" value="{{capacity}}" placeholder="0" style="width: 100px;" min="0">
 							</div>
 
-							<div id="course-classes" class="form-row" data-step="7" data-position="left" data-intro="Here shows a summary of the cabins available for this boat. To attach a cabin to a boat, click add cabin and select the cabin type and number of rooms">
-								<div id="class-types">
-								<h4>Classes for this course</h4>
-									{{#each classes}}
-										{{> classes_show}}
+							<div id="course-tickets" class="form-row" data-step="3" data-position="left" data-intro="Now, select the tickets that you want to include in the package. Once you select another ticket, another drop down box will appear to allow you to add another ticket. If you are finished adding tickets, leave the last one blank.">
+								<strong>Select the class to be included in this course:</strong>
+
+								{{#if update}}
+									{{> class_template}}
+								{{else}}
+									{{> class_select}}
+								{{/if}}
+							</div>
+
+							<div id="course-tickets" class="form-row ticket-list" data-step="3" data-position="left" data-intro="Now, select the tickets that you want to include in the package. Once you select another ticket, another drop down box will appear to allow you to add another ticket. If you are finished adding tickets, leave the last one blank.">
+								<strong>Select tickets to be included in this course:</strong>
+
+								{{#if update}}
+									{{> tickets_template}}
+								{{else}}
+									{{> ticket_select}}
+								{{/if}}
+							</div>
+
+							<div id="course-base" class="form-row">
+								<p><strong>Set base prices for this course:</strong></p>
+								{{#each base_prices}}
+									{{> price_input}}
+								{{/each}}
+								<button class="btn btn-success text-uppercase add-base-price"> &plus; Add base price</button>
+							</div>
+
+							<div id="course-seasonal" class="form-row">
+								<label>
+									<input type="checkbox" onchange="showMe('#seasonal-prices-list', this);"{{#if prices}} checked{{/if}}>
+									Add seasonal price changes?
+								</label>
+								<div class="dashed-border" id="seasonal-prices-list"{{#unless prices}} style="display: none;"{{/unless}}>
+									<h3>Seasonal price changes</h3>
+									{{#each prices}}
+										{{> price_input}}
+									{{else}}
+										{{#with default_price}}
+											{{> price_input}}
+										{{/with}}
 									{{/each}}
+									<button class="btn btn-success text-uppercase add-price"> &plus; Add seasonal price</button>
 								</div>
-								<button id="add-class" class="btn btn-success text-uppercase"> &plus; Add class</button>
 							</div>
 
 							{{#if update}}
 								<input type="hidden" name="id" value="{{id}}">
 							{{/if}}
-							
 							<input type="hidden" name="_token">
 
 							<input type="submit" class="btn btn-primary btn-lg text-uppercase pull-right" id="{{task}}-course" value="SAVE">
@@ -70,6 +108,99 @@
 			</div>
 		</div>
 	</div><!-- .row -->
+
+	<script type="text/x-handlebars-template" id="ticket-select-template">
+		<p>
+			<select class="ticket-select">
+				<option value="0">Select a ticket</option>
+				{{#each available_tickets}}
+					<option value="{{id}}"
+						{{#if ../existing}}
+							{{selected ../../id}}
+						{{/if}}
+					>{{{name}}}</option>
+				{{/each}}
+			</select>
+
+			Quantity: &nbsp;<input type="number" class="quantity-input"{{#if pivot.quantity}} name="tickets[{{id}}][quantity]"{{else}} disabled{{/if}} value="{{pivot.quantity}}" min="1" step="1" style="width: 50px;">
+			{{!--
+			<span class="ticket-prices" data-default="-">
+				{{#if existing}}
+					{{#each prices}}
+						<span style="border: 1px solid lightgray; padding: 0.25em 0.5em;">{{from}} - {{until}}: {{currency}} {{multiply ../pivot.quantity decimal_price}}</span>
+					{{/each}}
+				{{else}}
+				-
+				{{/if}}
+			</span>
+			--}}
+		</p>
+	</script>
+
+	<script type="text/x-handlebars-template" id="tickets-template">
+		{{#each tickets}}
+			<p>
+				<big class="margin-right">{{{name}}}</big> Quantity: <big class="margin-right">{{pivot.quantity}}</big>
+			</p>
+		{{/each}}
+	</script>
+
+	<script type="text/x-handlebars-template" id="class-select-template">
+		<p>
+			<select id="class-select" class="class-select">
+				{{#each available_training}}
+					<option value="{{id}}">{{name}}</option>
+				{{/each}}
+			</select>
+			Number of sessions : 
+			<input type="number" name="training_quantity" value="{{capacity}}" placeholder="0" style="width: 100px;" min="0">
+			<input type="hidden" type="number" name="training_id" id="training_id">
+		</p>
+	</script>
+
+	<script type="text/x-handlebars-template" id="class-template">
+		<p>
+			<big class="margin-right">{{{training.name}}}</big> Quantity: <big class="margin-right">{{training_quantity}}</big>
+		</p>
+	</script>
+
+	<script type="text/x-handlebars-template" id="price-input-template">
+		<p{{#unless decimal_price}} class="new_price"{{/unless}}>
+			<span class="currency">{{currency}}</span>
+			{{#if decimal_price}}
+				<span class="amount">{{decimal_price}}</span>
+			{{else}}
+				<input type="number" id="acom-price" name="{{#if isBase}}base_{{/if}}prices[{{id}}][new_decimal_price]" placeholder="00.00" min="0" step="0.01" style="width: 100px;">
+			{{/if}}
+
+			{{#unless isAlways}}
+				{{#if decimal_price}}
+					from <big>{{from}}</big>
+				{{else}}
+					from <input type="text" name="{{#if isBase}}base_{{/if}}prices[{{id}}][from]" class="datepicker" data-date-format="YYYY-MM-DD" value="{{from}}" style="width: 125px;">
+				{{/if}}
+			{{else}}
+				from <strong>the beginning of time</strong>
+				{{#unless decimal_price}}
+					<input type="hidden" name="{{#if isBase}}base_{{/if}}prices[{{id}}][from]" value="{{from}}">
+				{{/unless}}
+			{{/unless}}
+
+			{{#unless isBase}}
+				{{#if decimal_price}}
+					until <big>{{until}}</big>
+				{{else}}
+					until <input type="text" name="{{#if isBase}}base_{{/if}}prices[{{id}}][until]" class="datepicker" data-date-format="YYYY-MM-DD" value="{{until}}" style="width: 125px;">
+				{{/if}}
+			{{/unless}}
+
+			{{#unless isAlways}}
+				{{#unless decimal_price}}
+					<button class="btn btn-danger remove-price">&#215;</button>
+				{{/unless}}
+			{{/unless}}
+		</p>
+	</script>
 
 	<script type="text/x-handlebars-template" id="errors-template">
 		<div class="yellow-helper errors" style="color: #E82C0C;">
@@ -82,20 +213,8 @@
 		</div>
 	</script>
 
-	<script type="text/x-handlebars-template" id="class-select-template">
-		<p>
-			<select class="class-select" onchange="">
-				{{#each classes}}
-					<option value="{{id}}">{{name}}</option>
-				{{/each}}
-			</select>
-			Number of sessions:
-			<input type="number" name="class[{{id}}]" value="{{capacity}}" placeholder="0" style="width: 100px;" min="0">
-			<button class="btn btn-danger remove-class">&#215;</button>
-		</p>
-	</script>
-
-	<script src="/js/Controllers/Class.js"></script>
+	<script src="/js/Controllers/Ticket.js"></script>
 	<script src="/js/Controllers/Course.js"></script>
+	<script src="/js/Controllers/Class.js"></script>
 	<script src="/tabs/courses/js/script.js"></script>
 </div>
--- public_html/dashboard/tabs/courses/js/script.js
+++ public_html/dashboard/tabs/courses/js/script.js
@@ -1,51 +1,83 @@
-var courseForm;
-var courseList;
-window.classes;
+var courseForm,
+    courseList,
+    priceInputTemplate,
+    ticketSelectTemplate;
 
-Handlebars.registerHelper('readable', function(duration) {
-	return readableDuration(duration);
+// Needs to be declared before the $(function) call
+Handlebars.registerHelper('selected', function(ticketID) {
+	if(this.id == ticketID)
+		return ' selected';
+	else
+		return '';
 });
+Handlebars.registerHelper('multiply', function(a, b) {
+	return (a * b).toFixed(2);
+});
+Handlebars.registerHelper('count', function(array) {
+	var sum = 0;
+	_.each(array, function(value) {
+		sum += value.pivot.quantity * 1;
+	});
+	return sum;
+});
+Handlebars.registerHelper('currency', function() {
+	return window.company.currency.symbol;
+});
+Handlebars.registerPartial('tickets_template', $('#tickets-template').html());
+Handlebars.registerPartial('ticket_select', $('#ticket-select-template').html());
 
-Handlebars.registerPartial('classes_show', $('#class-select-template').html());
+Handlebars.registerPartial('class_template', $('#class-template').html());
+Handlebars.registerPartial('class_select', $('#class-select-template').html());
 
-$(function () {
+priceInputTemplate = Handlebars.compile( $('#price-input-template').html() );
+Handlebars.registerPartial('price_input', priceInputTemplate);
 
-	// Handlebars Prep
+window.sw.default_first_base_price = {
+	id: randomString(),
+	from: '0000-00-00',
+	isBase: true,
+	isAlways: true,
+};
+window.sw.default_base_price = {
+	isBase: true,
+	from: moment().format('YYYY-MM-DD'),
+};
+window.sw.default_price = {
+	id: randomString(),
+	from: moment().format('YYYY-MM-DD'),
+	until: moment().add(3, 'months').format('YYYY-MM-DD'),
+};
+
+$(function(){
+
+	// Render initial package list
 	courseList = Handlebars.compile( $("#course-list-template").html() );
-	courseForm = Handlebars.compile( $("#course-form-template").html() );
-
 	renderCourseList();
-	renderEditForm();
 
 	Class.getAll(function success(data) {
-		window.classes = data;
-		console.log(data);
+		window.training = _.indexBy(data, 'id');
+		console.log(window.training);
 	});
 
-	$('#course-list-container').on('click', 'li', function(event) {
+	// Default view: show create package form
+	Ticket.getAllTickets(function success(data) {
+		window.tickets = _.indexBy(data, 'id');
 
-		if( $(event.target).is('strong') ) event.target = event.target.parentNode;
-		renderEditForm( event.target.getAttribute('data-id') );
+		courseForm = Handlebars.compile( $("#course-form-template").html() );
+		renderEditForm();
+		//Tour.getCoursesTour();
 	});
 
-	$('#course-form-container').on('click', '#add-class', function(event) {
-		event.preventDefault();
-		var courseSelect = Handlebars.compile( $("#class-select-template").html() );
-		$("#class-types").append(courseSelect({classes : window.classes}));
-	});
-
-	$("#course-form-container").on('click', '.remove-class', function(event){
-		event.preventDefault();
-		$('form').data('hasChanged', true);
-		$(event.target).parent().remove();
-	});
+	ticketSelectTemplate = Handlebars.compile( $("#ticket-select-template").html() );
 
 	$("#course-form-container").on('submit', '#add-course-form', function(event) {
 
 		event.preventDefault();
 
 		// Show loading indicator
-		//$('#add-ticket').prop('disabled', true).after('<div id="save-loader" course="loader"></div>');
+		$('#add-course').prop('disabled', true).after('<div id="save-loader" class="loader"></div>');
+
+		$('#training_id').val(parseInt($("#class-select").val()));
 
 		Course.create( $('#add-course-form').serialize(), function success(data) {
 
@@ -53,7 +85,7 @@ $(function () {
 
 			$('form').data('hasChanged', false);
 
-			rendercourseList(function() {
+			renderCourseList(function() {
 				renderEditForm(data.id);
 			});
 
@@ -79,7 +111,7 @@ $(function () {
 			pageMssg('Oops, something wasn\'t quite right');
 
 			$('#add-course').prop('disabled', false);
-			//$('#add-course-form').find('#save-loader').remove();
+			$('#add-course-form').find('#save-loader').remove();
 		});
 	});
 
@@ -88,20 +120,36 @@ $(function () {
 		event.preventDefault();
 
 		// Show loading indicator
-		//$('#add-ticket').prop('disabled', true).after('<div id="save-loader" course="loader"></div>');
+		$('#update-course').prop('disabled', true).after('<div id="save-loader" class="loader"></div>');
+
+		$('#training_id').val(parseInt($("#class-select").val()));
 
 		Course.update( $('#update-course-form').serialize(), function success(data) {
 
 			pageMssg(data.status, true);
 
-			console.log(data);
-
 			$('form').data('hasChanged', false);
 
-			rendercourseList(function() {
-				renderEditForm(data.id);
-			});
+			renderCourseList();
 
+			$('.new_price').remove();
+
+			if(data.base_prices) {
+				_.each(data.base_prices, function(price) {
+					price.isBase = true;
+					$('.add-base-price').before( priceInputTemplate(price) );
+				});
+			}
+
+			if(data.prices) {
+				_.each(data.prices, function(price) {
+					$('.add-price').before( priceInputTemplate(price) );
+				});
+			}
+
+			// Remove the loader
+			$('#update-course').prop('disabled', false);
+			$('.loader').remove();
 		}, function error(xhr) {
 
 			var data = JSON.parse(xhr.responseText);
@@ -114,8 +162,8 @@ $(function () {
 
 				// Render error messages
 				$('.errors').remove();
-				$('#add-course-form').prepend(errorsHTML);
-				$('#add-course').before(errorsHTML);
+				$('#update-course-form').prepend(errorsHTML);
+				$('#update-course').before(errorsHTML);
 			}
 			else {
 				alert(xhr.responseText);
@@ -123,26 +171,146 @@ $(function () {
 
 			pageMssg('Oops, something wasn\'t quite right');
 
-			$('#add-course').prop('disabled', false);
-			//$('#add-course-form').find('#save-loader').remove();
+			$('#update-course').prop('disabled', false);
+			$('.loader').remove();
 		});
+	});
+
+	$('#course-form-container').on('change', '.ticket-select', function(event) {
+		var $self     = $(event.target);
+		var $quantity = $self.siblings('.quantity-input').first();
+		var $prices   = $self.siblings('.ticket-prices').first();
+
+		var id = $self.val(), disabledInputs, numberOfDisabledInputs;
+
+		if(id == "0") {
+			// Reset
+			$quantity.prop('disabled', true);
+			$quantity.attr('name', '');
+			$quantity.val('');
+
+			$prices.html( $prices.attr('data-default') );
+
+			// Check if more than one empty ticket-selects exist and if so, remove the extra one
+			disabledInputs         = $('.ticket-list').find('.quantity-input[disabled]');
+			numberOfDisabledInputs = disabledInputs.length;
+			if( numberOfDisabledInputs > 1) {
+				disabledInputs.last().parent().remove();
+			}
+		}
+		else {
+			$quantity.prop('disabled', false);
+			$quantity.attr('name', 'tickets[' + id + '][quantity]');
+			$quantity.val(1);
+
+			$quantity.trigger('change');
+
+			// Check if empty ticket-select exists and if not, create and append one
+			disabledInputs         = $('.ticket-list').find('.quantity-input[disabled]');
+			numberOfDisabledInputs = disabledInputs.length;
+			if( numberOfDisabledInputs === 0) {
+				$('.ticket-list').append( ticketSelectTemplate({available_tickets: window.tickets}) );
+			}
+		}
+	});
+
+	$('#course-form-container').on('change', '.quantity-input', function(event) {
+		var $quantity = $(event.target);
+		var $prices   = $quantity.siblings('.ticket-prices').first();
+		var $ticket   = $quantity.siblings('.ticket-select').first();
+		var id = $ticket.val();
+
+		/*
+		var html = '';
+		_.each(window.tickets[id].prices, function(p, index, list) {
+			html += '<span style="border: 1px solid lightgray; padding: 0.25em 0.5em;">' + p.fromDay + '/' + p.fromMonth + ' - ' + p.untilDay + '/' + p.untilMonth + ': ' + window.company.currency.symbol + ' ' + ($quantity.val() * p.decimal_price).toFixed(2) + '</span> ';
+		});
+
+		$prices.html(html);
+		*/
+	});
+
+	/*$('#course-form-container').on('click', '.remove-course', function(event) {
+    event.preventDefault();
+		var check = confirm('Do you really want to remove this course?');
+		if(check){
+			// Show loading indicator
+			$(this).prop('disabled', true).after('<div id="save-loader" class="loader"></div>');
+
+			Course.delete({
+				'id'    : $('#update-package-form input[name=id]').val(),
+				'_token': $('[name=_token]').val()
+			}, function success(data){
+
+				pageMssg(data.status, true);
+
+				renderPackageList();
+
+				renderEditForm();
+			}, function error(xhr){
+
+				pageMssg('Oops, something wasn\'t quite right');
+
+				$('.remove-package').prop('disabled', false);
+				$('#save-loader').remove();
+			});
+		}
+	});*/
+
+	$("#course-list-container").on('click', '#change-to-add-course', function(event){
+
+		event.preventDefault();
+		renderEditForm();
+	});
+
+	$('#course-form-container').on('click', '.add-base-price', function(event) {
+		event.preventDefault();
+
+		window.sw.default_base_price.id = randomString();
+		$(event.target).before( priceInputTemplate(window.sw.default_base_price) );
+		initPriceDatepickers();
+	});
+
+	$('#course-form-container').on('click', '.add-price', function(event) {
+		
+		event.preventDefault();
+		window.sw.default_price.id = randomString();
+		$(event.target).before( priceInputTemplate(window.sw.default_price) );
+		initPriceDatepickers();
+	});
+
+	$('#course-form-container').on('click', '.remove-price', function(event) {
+		
+		event.preventDefault();
+		$(event.target).parent().remove();
 	});
 
 });
 
 function renderCourseList(callback) {
 
-	//$('#course-list-container').append('<div id="save-loader" course="loader" style="margin: auto; display: block;"></div>');
+	$('#course-list-container').append('<div id="save-loader" class="loader" style="margin: auto; display: block;"></div>');
 
 	Course.getAll(function success(data) {
 
 		window.courses = _.indexBy(data, 'id');
+		console.log(window.courses);
 		$('#course-list').remove();
-		//$('#course-list-container .loader').remove();
+		$('#course-list-container .loader').remove();
 
 		$("#course-list-container").append( courseList({courses : data}) );
 
-		if(typeof callback === 'function') callback();
+		// (Re)Assign eventListener for package clicks
+		$('#course-list').on('click', 'li, strong', function(event) {
+
+			if( $(event.target).is('strong') )
+				event.target = event.target.parentNode;
+
+			renderEditForm( event.target.getAttribute('data-id') );
+		});
+
+		if( typeof callback === 'function')
+			callback();
 	});
 }
 
@@ -155,29 +323,47 @@ function renderEditForm(id) {
 		}
 	}
 
-	var training;
+	var course;
 
 	if(id) {
-		training = window.courses[id];
-		training.task         = 'update';
-		training.update       = true;
+		course = window.courses[id];
+
+		course.task   = 'update';
+		course.update = true;
+
+		_.each(course.tickets, function(value) {
+			value.existing = true;
+			value.available_tickets = window.tickets;
+		});
+
+		_.each(course.base_prices, function(value) {
+			value.isBase   = true;
+
+			if(value.from == '0000-00-00')
+				value.isAlways = true;
+		});
 	}
 	else {
-		training = {
+		// Set defaults for a new package form
+		course = {
 			task: 'add',
 			update: false,
-			hasBoats: false,
-			hasBoatrooms: false,
 			base_prices: [ window.sw.default_first_base_price ],
 		};
 	}
 
-	$('#course-form-container').empty().append( courseForm(training) );
+	course.available_tickets = window.tickets;
+	course.available_training = window.training;
+	course.default_price     = window.sw.default_price;
+
+	$('#course-form-container').empty().append( courseForm(course) );
 
 	if(!id)
 		$('input[name=name]').focus();
 
 	CKEDITOR.replace( 'description' );
+
+	initPriceDatepickers();
 
 	setToken('[name=_token]');
 
@@ -191,30 +377,16 @@ function unsavedChanges() {
 	return $('form').data('hasChanged');
 }
 
-function readableDuration(hours) {
-	var duration = moment.duration(parseInt(hours), 'hours');
-	return /*(duration.days() > 0 ?*/ duration.days() + ' d ' /*: '')*/ + duration.hours() + ' h';
-}
+function showMe(box, self) {
 
-function clearForm() {
+	var div = $(box);
 
-	var training;
-
-	training = {
-		task: 'add',
-		update: false
-	};
-
-	$('#course-form-container').empty().append( courseForm(training) );
-
-	$('input[name=name]').focus();
-
-	CKEDITOR.replace( 'description' );
-
-	setToken('[name=_token]');
-
-	// Set up change monitoring
-	$('form').on('change', 'input, select, textarea', function() {
-		$('form').data('hasChanged', true);
-	});
+	if( $(self).is(':checked') ) {
+		div.show(0);
+		div.find('input, select').prop('disabled', false);
+	}
+	else {
+		div.hide(0);
+		div.find('input, select').prop('disabled', true);
+	}
 }
