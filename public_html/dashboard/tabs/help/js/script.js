f3f167bfc6d391433375a6c26a23796389a2bd09
tparis0022 <thomas@scubawhere.com>
2015-04-18 15:34:33 +0000

    HTML formatting on multiple lines - Here's to hoping!

--- public_html/dashboard/tabs/help/js/script.js
+++ public_html/dashboard/tabs/help/js/script.js
@@ -5,17 +5,11 @@
 var faq = [
 	{
 		question: "How do I schedule a trip?",
-		answer: "Calendar > Scheduling
-		
-		Select a trip you wish to schedule, drag and drop the trip to the date you want to schedule the trip for."
+		answer: "<p>Calendar > Scheduling</p><p>Select a trip you wish to schedule, drag and drop the trip to the date you want to schedule the trip for.</p>"
 	},
 	{
 		question: "How do I timetable a trip?",
-		answer: "Calendar > Scheduling
-		
-		Select a trip you wish to schedule, drag and drop the trip to the date you want to schedule the trip for.
-		
-		Once the trip is on the calendar, double click on the trip you wish to schedule. Then you can create your trip timetable schedule. By default, any timetable will repeat over 1 year and 6 months unless you specify when the timetable should end."
+		answer: "<p>Calendar > Scheduling</p><p>Select a trip you wish to schedule, drag and drop the trip to the date you want to schedule the trip for.</p><p>Once the trip is on the calendar, double click on the trip you wish to schedule. Then you can create your trip timetable schedule. By default, any timetable will repeat over 1 year and 6 months unless you specify when the timetable should end.</p>"
 	},
 	{
 		question: "What if I don’t have a boat and operate dives from a pontoon/ beach or something similar?",
@@ -35,26 +29,15 @@ var faq = [
 	},
 	{
 		question: "How do I cancel a Reservation?",
-		answer: "Manage Bookings > Search for Reservation > Expand Reservation Details > Transaction
-		
-		Under the refund section of that reservation transaction, you will need to declare how the customer is being refunded. To cancel the reservation, ensure you check the ‘Cancel Booking’ checkbox. You can also cancel a reservation by clicking the ‘Cancel’ button when you expand the reservation details. However, you will still need to refund the customer to ensure you do not create a discrepancy between scubawhereRMS and your bank account."
+		answer: "<p>Manage Bookings > Search for Reservation > Expand Reservation Details > Transaction</p><p>Under the refund section of that reservation transaction, you will need to declare how the customer is being refunded. To cancel the reservation, ensure you check the ‘Cancel Booking’ checkbox. You can also cancel a reservation by clicking the ‘Cancel’ button when you expand the reservation details. However, you will still need to refund the customer to ensure you do not create a discrepancy between scubawhereRMS and your bank account.</p>"
 	},
 	{
 		question: "How do I edit a Reservation?",
-		answer: "Manage Bookings > Search for Reservation > Expand Reservation Details > View & Edit
-		
-		When you click on View and Edit, the reservation is returned to the Add Booking process, where you can edit the booking accordingly and proceed as if it was a new booking."
+		answer: "<p>Manage Bookings > Search for Reservation > Expand Reservation Details > View & Edit</p><p>When you click on View and Edit, the reservation is returned to the Add Booking process, where you can edit the booking accordingly and proceed as if it was a new booking.</p>"
 	},
 	{
 		question: "How do I update my Travel Agents?",
-		answer: "Management > Agents
-		
-		You can update the agent who supply you with bookings when you click on the agent. Remember to click ‘Save’.
-		
-		In a re-cap of Business Terms,
-		Full Amount means the agent takes the full amount from the customer, which you can send an invoice to or as per your agreed arrangement to getting paid from the agent.
-		Deposit Only means the agent purely takes their commission and you need to collect the remaining balance from the customer directly.
-		Banned means you no longer are accepting bookings from that agent and effectively block them from generating you custom."
+		answer: "<p>Management > Agents</p><p>You can update the agent who supply you with bookings when you click on the agent. Remember to click ‘Save’.</p><p>In a re-cap of Business Terms,</p><p>Full Amount means the agent takes the full amount from the customer, which you can send an invoice to or as per your agreed arrangement to getting paid from the agent.</p><p>Deposit Only means the agent purely takes their commission and you need to collect the remaining balance from the customer directly.</p><p>Banned means you no longer are accepting bookings from that agent and effectively block them from generating you custom.</p>"
 	},
 	{
 		question: "I have more questions and would like to speak to someone",
