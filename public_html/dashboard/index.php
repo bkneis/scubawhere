05c312b1b73693bdda309ef607a32d1176c93ee6
bkneis <bryan@iqwebcreations.com>
2015-07-12 09:17:47 -0700

    Added border to notifications

--- public_html/dashboard/index.php
+++ public_html/dashboard/index.php
@@ -147,8 +147,9 @@ $PROTOCOL = 'http';
 			<div class="nav-opt pull-right"><a href="#settings" class="username"></a></div>
 
 			<div class="notifications pull-right">
-				<i style="padding-left:8px; padding-top:8px; cursor:pointer" class="fa fa-bell fa-lg fa-fw"></i>
-				<div id="notification-messages" class="messages"></div>
+				<i style="padding-left:8px; padding-top:9px; cursor:pointer" class="fa fa-bell fa-lg fa-fw"></i>
+				<div id="notification-messages" style="border: 2px solid #34495e;" class="messages">
+				</div>
 			</div>
 		</div>
 	</div>
