b3fad7cb8a536442d828aa1060d62860bcedf298
bkneis <bryan@iqwebcreations.com>
2015-09-06 04:04:17 -0700

    Data models for crm groups and campaigns

---
+++ app/models/CrmCampaign.php
@@ -0,0 +1,36 @@
+<?php
+
+use LaravelBook\Ardent\Ardent;
+use ScubaWhere\Helper;
+
+class CrmCampaign extends Ardent {
+
+	protected $table = 'company_campaigns';
+
+	protected $guarded = array('id', 'company_id', 'created_at', 'updated_at');
+
+	public static $rules = array(
+		'subject'        => 'required',
+		'message' 		 => 'required',
+		'num_sent'		 => ''
+	);
+
+	public function beforeSave( $forced )
+	{
+
+		if( isset($this->name) )
+			$this->name = Helper::sanitiseString($this->name);
+
+	}
+
+	public function company()
+	{
+		return $this->belongsTo('Company');
+	}
+
+	public function groups()
+	{
+		return $this->belongsToMany('CrmGroup');
+	}
+
+}
