1c2cd6ff867d60c74fa97baaaa41cee9c040d428
Thomas Paris <thomas@scubawhere.com>
2015-09-11 10:23:22 +0000

    index.php edited online with Bitbucket

--- public_html/dashboard/tabs/tickets/index.php
+++ public_html/dashboard/tabs/tickets/index.php
@@ -81,7 +81,7 @@
 							{{/unless}}
 
 							<div class="form-row" id="tickets-trips" data-step="3" data-position="top" data-intro="Select which trips a ticket can be used for. The ticket is valid for only 1 trip.">
-								<h4>Please select the trips that this ticket should be eligable for:</h4>
+								<h4>Please select the trips that this ticket should be eligible for:</h4>
 								<div id="ticket-selectList" class="form-row">
 								{{#each available_trips}}
 										<label class="ticket-select{{inArray id ../trips ' checked' ''}}">
