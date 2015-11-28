ca059fa76edb350aab4eb86bede15fc5ab5b229d
Bryan Kneis <bryan@iqwebcreations.com>
2015-11-28 17:45:43 +0000

    Created new email editor

---
+++ public_html/dashboard/email-editor/index.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+    <title>scubawhereRMS | Dashboard</title>
+
+    <!-- favicon -->
+    <link rel="icon" type="image/ico" href="../common/favicon.ico" />
+
+    <!--Bootstrap CSS-->
+    <link rel="stylesheet" type="text/css" href="../common/css/bootstrap-scubawhere.css" />
+
+    <!-- scubawhere styles -->
+    <link rel="stylesheet" type="text/css" href="../common/css/universal-styles.css" />
+    <link rel="stylesheet" type="text/css" href="../css/style.css" />
+
+    <!-- Plugins -->
+    <link rel='stylesheet' type="text/css" href='../common/css/fullcalendar.min.css' />
+    <link rel="stylesheet" type="text/css" href="../common/css/jquery.reveal.css" />
+    <link rel="stylesheet" type="text/css" href="../common/vendor/font-awesome/css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="../common/css/bootstrap-datetimepicker.css" />
+    <link rel="stylesheet" type="text/css" href="../common/css/select2.css" />
+    <link rel="stylesheet" type="text/css" href="../common/css/select2-bootstrap.css" />
+    <link rel="stylesheet" type="text/css" href="../common/vendor/nprogress/nprogress.css" />
+    <link rel="stylesheet" type="text/css" href="../common/css/bootstrap.datatables.css" />
+    <link rel="stylesheet" type="text/css" href="../common/vendor/datatables-tabletools/css/dataTables.tableTools.css" />
+
+    <!-- jQuery -->
+    <script src="../common/js/jquery/jquery.min.js"></script>
+
+    <!--Bootstrap js-->
+    <script type="text/javascript" src="../common/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- other -->
+    <script type="text/javascript" src="../common/js/handlebars.min.js"></script>
+    <script type="text/javascript" src="../common/js/underscore-min.js"></script>
+    <script type="text/javascript" src="../common/js/jquery/jquery.serialize-object.min.js"></script>
+    <script type="text/javascript" src="../common/js/jquery/jquery.reveal.js"></script>
+    <script type="text/javascript" src="../common/js/moment.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+
+    <!-- scubawhere files -->
+    <script type="text/javascript">
+        // Set scubawhere namespace
+        window.sw = {};
+        window.promises = {};
+    </script>
+
+    <script type="text/javascript" src="/js/main.js"></script>
+    <script type="text/javascript" src="/js/ui.js"></script>
+
+    <!-- Controllers -->
+    <script type="text/javascript" src="../js/Controllers/Campaign.js"></script>
+    <script type="text/javascript" src="../js/Controllers/CustomerGroup.js"></script>
+    <script type="text/javascript" src="../js/Controllers/Company.js"></script>
+
+    <!--Datetimepicker-->
+    <script type="text/javascript" src="../common/js/bootstrap-datetimepicker.min.js"></script>
+
+    <!--Select 2-->
+    <script type="text/javascript" src="../common/js/select2.min.js"></script>
+
+    <!--nprogress bar-->
+    <script type="text/javascript" src="../common/vendor/nprogress/nprogress.js"></script>
+
+    <meta http-equiv="X-UA-Compatible" content="IE=9">
+
+</head>
+
+<body>
+    <div id="nav">
+        <div id="nav-wrapper">
+            <h1 id="logo"><a href="/"><img src="/common/img/Scubawhere_logo.png"></a></h1>
+
+            <button class="btn btn-default pull-right" id="logout">Logout</button>
+
+            <div class="nav-opt pull-right">
+                <a href="#settings" class="username"></a>
+            </div>
+
+        </div>
+    </div>
+
+    <!-- Container for page messages -->
+    <div id="pageMssg"></div>
+
+    <div id="modalWindows" style="height: 0;"></div>
+
+    <iframe id="email-template-editor" style="position: fixed; border: none; top: 50px; right: 0; bottom: 0; left: 0; width: 100%; height: 100%;"></iframe>
+
+    <div style="position: fixed; bottom: 0; width: 100%; height:80px" class="alert-warning clearfix">
+        <div class="alert pull-right">
+            <div class="pull-left">
+                <button id="choose-template" class="btn btn-primary btn-lg pull-left">Choose Different Template</button>
+            </div>
+            <div class="pull-right">
+                <button id="send-email" class="btn btn-primary btn-lg pull-right">SEND</button>
+                <button id="save-as-template" class="btn btn-success btn-lg pull-right" style="margin-right: 5px;">Save as Template</button>
+                <a href="/#campaigns" class="btn btn-default btn-lg abandon-booking pull-right" style="margin-right: 5px;">Discard Email</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modals -->
+    <div class="modal fade" id="select-email-template-modal">
+        <div class="modal-dialog" style="width:80%">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title">Choose Template</h4>
+                </div>
+                <div class="modal-body">
+                    <div id="template-types" class="btn-group" style="margin-bottom:10px" role="group">
+                        <button display="layout-options" type="button" class="btn btn-default btn-primary option-button">Layouts</button>
+                        <button display="template-options" type="button" class="btn btn-default option-button">My Templates</button>
+                    </div>
+                    <!-- loop through templates-->
+                    <div style="margin: 0 auto;">
+                        <div id="template-options" class="email-options-list" style="width:35%; float:left; max-height:620px; overflow: auto; display:none;">
+                            <ul id="layout-options-list" style="margin-right:10px" class="entity-list"></ul>
+                        </div>
+                        <div id="layout-options" style="width:35%; float:left; max-height:620px; overflow: auto" class="email-options-list">
+                            <img width="45%" height="200px" class="email-template-option" data-url="/tabs/campaigns/email-templates/base_boxed_2column_query.html" src="/img/email-templates/email_template_1.jpg">
+                            <img width="45%" height="200px" class="email-template-option" data-url="/tabs/campaigns/email-templates/base_boxed_3column_query.html" src="/img/email-templates/email_template_2.jpg">
+                            <img width="45%" height="200px" class="email-template-option" data-url="/tabs/campaigns/email-templates/base_boxed_basic_body_image_query.html" src="/img/email-templates/email_template_3.jpg">
+                            <img width="45%" height="200px" class="email-template-option" data-url="/tabs/campaigns/email-templates/base_boxed_basic_query.html" src="/img/email-templates/email_template_4.jpg">
+                            <img width="45%" height="200px" class="email-template-option" data-url="/tabs/campaigns/email-templates/base_boxed_body_image_2column_query.html" src="/img/email-templates/email_template_5.jpg">
+                        </div>
+                        <div style="width:65%; float:left">
+                            <h4>Preview:</h4>
+                            <iframe id="email-template-option-preview" width="100%" height="600px" style="border:none"></iframe>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button id="select-email-template" type="submit" class="btn btn-primary">Use Template</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="save-email-template-modal">
+        <div class="modal-dialog" style="width:40%">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title">Save Template</h4>
+                </div>
+                <div style="height:240px" class="modal-body">
+                    <label for="template-name" class="control-label">Template name :</label>
+                    <input type="text" id="template-name" class="form-control">
+                    <div style="margin-top:10px; margin-bottom:10px;" class="alert alert-warning">
+                        <p><strong>Where to find your templates</strong></p>
+                        <p>Once saved, you can access and edit this template by clicking select email template, then go to 'My templates'</p>
+                    </div>
+                    <button id="btn-save-template" style="margin-top:10px" class="btn btn-primary btn-lg text-uppercase pull-right">SAVE</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="send-email-modal">
+        <div class="modal-dialog" style="width:60%">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title">Send Email</h4>
+                </div>
+                <div class="modal-body">
+                    <form id="create-campaign-form" accept-charset="utf-8">
+                        <div class="form-row">
+                            <label class="field-label">Campaign name</label>
+                            <input id="campaign-name" type="text" name="name" style="width:100%">
+                        </div>
+                        <fieldset id="add-customer-group-to-campaign">
+                            <div class="form-group">
+                                <div class="col-md-10">
+                                    <label for="customer_group_id" class="control-label">Which groups are you sending this to?</label>
+                                    <select id="customer_group_id" class="form-control select2">
+                                    </select>
+                                </div>
+                                <div class="col-md-2">
+                                    <label>&nbsp;</label>
+                                    <br>
+                                    <button class="btn btn-primary add-group" style="width: 100%;">Add</button>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-md-12" style="padding-top:10px" id="selected-customer-groups">
+                                </div>
+                            </div>
+                        </fieldset>
+                        <div class="form-row">
+                            <label class="field-label">Email Subject</label>
+                            <input id="email_subject" type="text" name="subject" style="width:100%">
+                        </div>
+                        <button id="cancel-email" class="btn btn-danger btn-lg text-uppercase">CANCEL</button>
+                        <input type="submit" class="btn btn-success btn-lg text-uppercase pull-right" value="SEND">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Handlebars templates -->
+    <script type="text/x-handlebars-template" id="layout-options-list-template">
+        {{#each layout}}
+        <li class="email-layout-option" data-html="{{html_string}}">{{name}}</li>
+        {{/each}}
+    </script>
+
+    <script type="text/x-handlebars-template" id="group-select-template">
+        <option value="">Choose group...</option>
+        {{#each groups}}
+        <option value="{{id}}">{{name}}</option>
+        {{/each}}
+    </script>
+
+    <script type="text/x-handlebars-template" id="selected-group-template">
+        <div class="pull-left selected-certificate">
+            <input type="checkbox" name="groups[]" value="{{id}}" style="position: absolute; top: 0; left: -9999px;" checked="checked">
+            <strong>{{{name}}}</strong>
+            <i class="fa fa-times remove-group" style="cursor: pointer;"></i>
+        </div>
+    </script>
+
+</body>
+
+</html>
---
+++ public_html/dashboard/email-editor/js/script.js
@@ -0,0 +1,197 @@
+var optionListTemplate;
+var groupSelectTemplate;
+var selectedCustomerGroupTemplate;
+var email_created = false;
+window.token;
+$(function () {
+
+    optionListTemplate = Handlebars.compile($("#layout-options-list-template").html());
+    groupSelectTemplate = Handlebars.compile($('#group-select-template').html());
+    selectedCustomerGroupTemplate = Handlebars.compile($('#selected-group-template').html());
+    
+    window.token = getToken();
+
+    $('#save-as-template').on('click', function (event) {
+        event.preventDefault();
+        if(!email_created) {
+            pageMssg('Please create an email to save it as a template', false);
+            return false;
+        }
+        $('#save-email-template-modal').modal('show');
+        $('#btn-save-template').on('click', function (event) {
+            event.preventDefault();
+            var params = {};
+            params.html_string = document.getElementById("email-template-editor").contentWindow.document.documentElement.outerHTML;
+            params.name = $('#template-name').val();
+            params._token = window.token;
+            Campaign.createTemplate(params, function success(data) {
+                    console.log(data);
+                    pageMssg(data.status, true);
+                    $('#template-name').val('');
+                    $('#save-email-template-modal').modal('hide');
+                    window.location.href = "/#campaigns";
+                },
+                function error(xhr) {
+                    console.log(xhr);
+                    pageMssg(xhr.responseText, false);
+                });
+        });
+    });
+
+    $('#send-email').on('click', function (event) {
+        event.preventDefault();
+        if(!email_created) {
+            pageMssg('Please create an email before trying to send', false);
+            return false;
+        }
+        $('#send-email-modal').modal('show');
+        CustomerGroup.getAll(function success(data) {
+            window.groups = _.indexBy(data, 'id');
+            $("#add-customer-group-to-campaign").find('#customer_group_id').empty().append(groupSelectTemplate({
+                groups: data
+            }));
+        });
+    });
+    
+    $('#cancel-email').on('click', function(event) {
+        event.preventDefault();
+        $('#send-email-modal').modal('hide');
+    });
+
+    $('#choose-template').on('click', function (event) {
+        event.preventDefault();
+        showTemplates();
+    });
+
+    var email_template_html = '';
+    var email_preview_frame = document.getElementById('email-template-option-preview');
+    var email_editor_frame = document.getElementById('email-template-editor');
+    var using_layout = true;
+    var layout_html_string = '';
+
+    $('.option-button').on('click', function () {
+        $('.email-template-option').css('border', 'none');
+        $('.email-options-list').css('display', 'none');
+        $('#' + $(this).attr('display')).css('display', 'block');
+        $('.option-button').removeClass('btn-primary');
+        $(this).addClass('btn-primary');
+        $('.email-layout-option').on('click', function () {
+            layout_html_string = $(this).attr('data-html');
+            email_template_html = processEmailHtml(layout_html_string);
+            email_preview_frame.contentWindow.document.open();
+            email_preview_frame.contentWindow.document.write(email_template_html);
+            email_preview_frame.contentWindow.document.close();
+        });
+    });
+
+    $('.email-template-option').on('click', function () {
+        $('.email-template-option').css('border', 'none');
+        $(this).css('border', '3px solid #FF7163');
+        $.get($(this).attr('data-url'), function (response) {
+            email_template_html = processEmailHtml(response);
+            email_preview_frame.contentWindow.document.open();
+            email_preview_frame.contentWindow.document.write(email_template_html);
+            email_preview_frame.contentWindow.document.close();
+            using_layout = false;
+        });
+        selected_email_template = $(this).attr('data-url');
+    });
+
+    $('#select-email-template').on('click', function () {
+        $('#save-as-template').removeAttr('disabled');
+        $('#email-editor-tips').css('display', 'none');
+        $('#email-template-editor').css('display', 'inline');
+        $('#show-email-browser').css('display', 'inline');
+        if (using_layout) {
+            email_editor_frame.contentWindow.document.open();
+            email_editor_frame.contentWindow.document.write(layout_html_string);
+            email_editor_frame.contentWindow.document.close();
+        } else $('#email-template-editor').attr('src', selected_email_template);
+        $('#select-email-template-modal').modal('hide');
+        email_created = true;
+    });
+    
+    $('#add-customer-group-to-campaign').on('click', '.add-group', function(event) {
+        event.preventDefault(); // Prevent form submission (some browsers treat any <button> press in a form as a submit)
+
+        var self = $(this);
+        var group_dropdown = self.closest('fieldset').find('#customer_group_id');
+        console.log(group_dropdown.val());
+
+        if (group_dropdown.val() === "") return false;
+
+        self.closest('fieldset').find('#selected-customer-groups').append(selectedCustomerGroupTemplate({
+            id: group_dropdown.val(),
+            name: window.groups[group_dropdown.val()].name
+        }));
+    });
+
+    $('#add-customer-group-to-campaign').on('click', '.remove-group', function() {
+        $(this).parent().remove();
+    });
+    
+    $('#create-campaign-form').on('submit', function(event) {
+        event.preventDefault();
+        var params = $(this).serializeObject();
+        params.email_html = processEmailHtml();
+        params._token = window.token;
+        for (var i = 0; i < params.groups.length; i++) {
+            params.groups[i] = parseInt(params.groups[i]);
+        }
+        sendCampaign(params);
+    });
+
+    showTemplates();
+
+});
+
+function sendCampaign(params)
+{
+    Campaign.create(params, function success(data) {
+        console.log(data);
+        pageMssg(data.status, true);
+        window.location.href = '/#campaigns';
+    },
+    function error(xhr) {
+        console.log(xhr);
+        pageMssg(xhr.responseText, false);
+    });
+}
+
+function showTemplates() 
+{
+    Campaign.getAllTemplates(function success(data) {
+        console.log(data);
+        $('#layout-options-list').empty().append(optionListTemplate({
+            layout: data
+        }));
+        $('#select-email-template-modal').modal('show');
+    });
+}
+
+function processEmailHtml(html_string) 
+{
+
+    if (!html_string) {
+        var html_string = document.getElementById("email-template-editor").contentWindow.document.documentElement.outerHTML;
+    }
+
+    var script_pos = html_string.indexOf('<script type="text/javascript" src="/tabs/campaigns/email-templates/js/medium-editor.js"></script>');
+    var html_compiled = html_string.substring(0, script_pos) + '</body></html>';
+
+    var find = 'contenteditable="true"';
+    var re = new RegExp(find, 'g');
+    html_compiled = html_compiled.replace(re, '');
+
+    find = 'data-medium-editor-element="true"';
+    re = new RegExp(find, 'g');
+    html_compiled = html_compiled.replace(re, '');
+
+    find = 'role="textbox"';
+    re = new RegExp(find, 'g');
+    html_compiled = html_compiled.replace(re, '');
+
+    // ADD ABILITY TO REG EXP OUT THE INPUT FILE
+
+    return html_compiled;
+}
