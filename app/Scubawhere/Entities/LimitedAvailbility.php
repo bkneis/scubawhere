85c6340c02d3e699a02e473bd447728d60399045
Bryan Kneis <bryan@iqwebcreations.com>
2016-12-20 15:22:13 +0000

    Implement getters and setters for dates available for entities using the LimitedAvailability trait

--- app/Scubawhere/Entities/LimitedAvailbility.php
+++ app/Scubawhere/Entities/LimitedAvailbility.php
@@ -1,1 +1,31 @@
 <?php
+
+namespace Scubawhere\Entities;
+
+trait LimitedAvailability {
+
+    public function setAvailableFromAttribute($value)
+    {
+        $value = trim($value);
+        $this->attributes['available_from'] = $value ?: null;
+    }
+
+    public function setAvailableUntilAttribute($value)
+    {
+        $value = trim($value);
+        $this->attributes['available_until'] = $value ?: null;
+    }
+
+    public function setAvailableForFromAttribute($value)
+    {
+        $value = trim($value);
+        $this->attributes['available_for_from'] = $value ?: null;
+    }
+
+    public function setAvailableForUntilAttribute($value)
+    {
+        $value = trim($value);
+        $this->attributes['available_for_until'] = $value ?: null;
+    }
+    
+}
