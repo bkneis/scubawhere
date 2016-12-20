f4fe7b5bd516fac6dbf45994c111fc5a69084e3f
Bryan Kneis <bryan@iqwebcreations.com>
2016-12-20 16:46:51 +0000

    Add docblock to Course model

--- app/Scubawhere/Entities/Course.php
+++ app/Scubawhere/Entities/Course.php
@@ -14,7 +14,10 @@ class Course extends Ardent {
 	use Bookable;
 	use SoftDeletingTrait;
 	
-	protected $dates = ['deleted_at'];
+	protected $dates = array('deleted_at');
+	
+	/* @todo Remove this, its a waste of a trip to the DB */
+	public $appends = array('deleteable');
 
 	protected $fillable = array('name', 'description', 'capacity', 'certificate_id');
 
@@ -25,44 +28,33 @@ class Course extends Ardent {
 		'certificate_id' => 'integer|exists:certificates,id'
 	);
 
-    public $appends = array('deleteable');
-
+	/**
+	 * Sanitise the html and string inputs using Purifier
+     */
 	public function beforeSave()
 	{
-		if( isset($this->name) )
+		if (isset($this->name)) {
 			$this->name = Helper::sanitiseString($this->name);
-
-		if( isset($this->description) )
+		}
+		if (isset($this->description)) {
 			$this->description = Helper::sanitiseBasicTags($this->description);
+		}
     }
 
-    public function getDeleteableAttribute()
+	/** @return bool */
+	public function getDeleteableAttribute()
     {
         return !($this->packages()->exists());
     }
 
-	public function calculatePrice($start, $limitBefore = false)
-	{
-		$price = Price::where(Price::$owner_id_column_name, $this->id)
-		     ->where(Price::$owner_type_column_name, 'Scubawhere\Entities\Course')
-		     ->where('from', '<=', $start)
-		     ->where(function($query) use ($start)
-		     {
-		     	$query->whereNull('until')
-		     	      ->orWhere('until', '>=', $start);
-		     })
-		     ->where(function($query) use ($limitBefore)
-		     {
-		     	if($limitBefore)
-		     		$query->where('created_at', '<=', $limitBefore);
-		     })
-		     ->orderBy('id', 'DESC')
-			 ->withTrashed()
-		     ->first();
-
-		$this->decimal_price = $price->decimal_price;
-	}
-	
+	/**
+	 * Overload Eloquent's create method to also return a HTTP response on failure
+	 * 
+	 * @param array $data
+	 * @return \Illuminate\Database\Eloquent\Model
+	 * @throws HttpUnprocessableEntity
+	 * @throws \Exception
+     */
 	public static function create(array $data = [])
 	{
 		$course = new Course($data);
@@ -73,6 +65,8 @@ class Course extends Ardent {
 	}
 
 	/**
+	 * Syncronise the addons related to this package to the given array
+	 * 
 	 * @param array $data
 	 * @return $this
 	 * @throws HttpUnprocessableEntity
@@ -84,61 +78,16 @@ class Course extends Ardent {
 		}
 		return $this;
 	}
-
-	public function scopeOnlyOwners($query)
-	{
-		return $query->where('company_id', '=', Context::get()->id);
-	}
-
-	/*public function bookingdetails()
-	{
-		return $this->hasMany('\Scubawhere\Entities\Bookingdetail');
-	}*/
-
-	public function company()
-	{
-		return $this->belongsTo('\Scubawhere\Entities\Company');
-	}
-
-	public function packages()
-	{
-        return $this->morphToMany('\Scubawhere\Entities\Package', 'packageable')
-                    ->withPivot('quantity')
-                    ->withTimestamps();
-	}
-
-	/*public function basePrices()
-	{
-		return $this->morphMany('\Scubawhere\Entities\Price', 'owner')->whereNull('until');
-	}
-
-	public function prices()
-	{
-		return $this->morphMany('\Scubawhere\Entities\Price', 'owner')->whereNotNull('until');
-	}*/
-
-	public function tickets()
-	{
-		return $this->belongsToMany('\Scubawhere\Entities\Ticket')->withPivot('quantity')->withTimestamps();
-	}
-
-	public function trainings()
-	{
-        return $this->belongsToMany('\Scubawhere\Entities\Training')
-                    ->withPivot('quantity')
-                    ->withTimestamps();
-    }
-
+	
 	/**
 	 * Syncronise the tickets and trainings id's to a course.
 	 *
-	 * This function utilises laravel's sync method that ensures
-	 * the ids of the given array match the ones in the course_*
-	 * pivot tables.
+	 * This function utilises laravel's sync method that ensures the ids of 
+	 * the given array match the ones in the course_* pivot tables.
 	 *
 	 * @param $data
 	 * @return $this
-     */
+	 */
 	public function syncItems($data)
 	{
 		if (! is_null($data['tickets'])) {
@@ -149,4 +98,39 @@ class Course extends Ardent {
 		}
 		return $this;
 	}
+	
+	/**
+	 * |--------------------------------------
+	 * | Eloquent relationships
+	 * |--------------------------------------
+	 */
+
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+	public function tickets()
+	{
+		return $this->belongsToMany('\Scubawhere\Entities\Ticket')->withPivot('quantity')->withTimestamps();
+	}
+
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+	public function trainings()
+	{
+        return $this->belongsToMany('\Scubawhere\Entities\Training')
+                    ->withPivot('quantity')
+                    ->withTimestamps();
+    }
+	
+	/**
+	 * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+	 */
+	public function packages()
+	{
+		return $this->morphToMany('\Scubawhere\Entities\Package', 'packageable')
+			->withPivot('quantity')
+			->withTimestamps();
+	}
+
 }
