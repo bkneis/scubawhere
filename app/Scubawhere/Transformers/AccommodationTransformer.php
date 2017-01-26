2dd35bf684fb80dd899473551f0ca58abf53c7e5
Bryan Kneis <bryan@iqwebcreations.com>
2017-01-26 12:48:12 +0000

    Implement accommodation and price transformer to reduce uneeded data being ressponded by the API, such as company_id, update_at, created_at etc

---
+++ app/Scubawhere/Transformers/AccommodationTransformer.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Scubawhere\Transformers;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccommodationTransformer extends Transformer
+{
+    protected $priceTransformer;
+
+    public function __construct(PriceTransformer $priceTransformer)
+    {
+        $this->priceTransformer = $priceTransformer;
+    }
+
+    /**
+     * The need to merge base_prices and prices is that the eloquent relationships
+     * are used througout the application so cannot be changed without alot of rewrite,
+     * so to atleast allow the front end to be redesigned with only prices we merge them
+     * when transforming
+     * 
+     * @param $accommodation
+     * @return array
+     */
+    public function transform($accommodation)
+    {
+        if ($accommodation instanceof Model) {
+            $accommodation = $accommodation->toArray();
+        }
+        return array(
+            'id'          => $accommodation['id'],
+            'name'        => $accommodation['name'],
+            'description' => $accommodation['description'],
+            'capacity'    => $accommodation['capacity'],
+            'prices'      => array_merge(
+                                $this->priceTransformer->transformMany($accommodation['prices']),
+                                $this->priceTransformer->transformMany($accommodation['base_prices'])
+                            ),
+            'base_prices' => []
+        );
+    }
+}
