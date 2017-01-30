c713fee96f7b74782f360d2deca3072f799b34f0
Bryan Kneis <bryan@iqwebcreations.com>
2017-01-30 16:26:58 +0000

    Implement addon and agents tab similiar to accommodations v2

--- public_html/src/js/Tabs/Addons.vue
+++ public_html/src/js/Tabs/Addons.vue
@@ -1,14 +1,69 @@
 <template>
-    <div>
-        <p> {{ msg }} </p>
-    </div>
+    <management-tab v-if="promises.addons"
+                    model="Addons"
+                    :action="form.action"
+                    @create="form.create()">
+
+        <ul slot="entity-list" class="entity-list">
+
+            <li v-for="addon in addons"
+                :value="addon.id"
+                @click="form.update(addon)">
+                <strong>{{ addon.name }}</strong> | {{ priceRange(addon) }}
+            </li>
+
+        </ul>
+
+        <entity-form slot="entity-form"
+                     :form="form"
+                     @create="onCreate"
+                     @update="onUpdate"
+                     @delete="onDelete">
+
+            <in-text   label="Addon name :"
+                       v-model="form.name"
+                       :required="true"/>
+
+            <in-editor label="Addon description :"
+                       v-model="form.description"
+                       :required="false"/>
+
+            <in-prices label="Price per night :"
+                       v-model="form.prices"
+                       :required="true"/>
+
+        </entity-form>
+
+    </management-tab>
 </template>
 
-<script>
+<script type="text/babel">
+    import FormErrors from 'Services/FormErrors.ts';
+    import Form from 'Services/EntityForm.ts';
+    import ManagementTab from 'Mixins/ManagementTab.ts';
+    import PriceService from 'Services/PriceService.ts';
+
     export default {
+        mixins : [ ManagementTab('addons') ],
         data() {
             return {
-                msg : 'Addons'
+                form : new Form({
+                    resource : 'addon',
+                    action   : 'Create',
+                    errors   : new FormErrors(),
+                    data     : {
+                        id          : null,
+                        name        : '',
+                        description : '',
+                        prices      : [ PriceService.emptyPrice() ]
+                    }
+                })
+            }
+        },
+        methods: {
+            // @overload
+            priceRange() {
+                return 'dsfsdf';
             }
         }
     }
--- public_html/src/js/Tabs/Agents.vue
+++ public_html/src/js/Tabs/Agents.vue
@@ -1,14 +1,80 @@
 <template>
-    <div>
-        <p> {{ msg }} </p>
-    </div>
+    <management-tab v-if="promises.agents"
+                    model="Agents"
+                    :action="form.action"
+                    @create="form.create()">
+
+        <ul slot="entity-list" class="entity-list">
+
+            <li v-for="agent in agents"
+                :value="agent.id"
+                @click="form.update(agent)">
+                <strong>{{ agent.name }}</strong> | {{ agent.branch_name }}
+            </li>
+
+        </ul>
+
+        <entity-form slot="entity-form"
+                     :form="form"
+                     @create="onCreate"
+                     @update="onUpdate"
+                     @delete="onDelete">
+
+            <in-text label="Agent Name:" v-model="form.name" :required="true"/>
+            <in-text label="Agent's Website:" v-model="form.website"/>
+            <in-text label="Branch Name:" v-model="form.branch_name" :required="true"/>
+            <in-editor label="Branch Address:" v-model="form.branch_address" :required="true"/>
+            <in-text label="Branch Telephone:" v-model="form.branch_telephone"/>
+            <in-text label="Branch Email:" v-model="form.email"/>
+            <in-check label="Does the agent have different billing details?" v-model="hasDifferentBillingDetails"/>
+            <div class="form-row">
+                <label>
+                    <input type="checkbox" v-model="hasDifferentBillingDetails">
+                    Does the agent have different billing details?
+                </label>
+            </div>
+            <div v-if="hasDifferentBillingDetails" class="dashed-border">
+                <h3>Billing Information</h3>
+            </div>
+            <in-number label="Commission (%):" v-model="form.commission" :required="true"/>
+            <in-select label="Terms of business with the agent:" :options="businessTerms"/>
+
+        </entity-form>
+
+    </management-tab>
 </template>
 
-<script>
+<script type="text/babel">
+    import Form from 'Services/EntityForm.ts';
+    import FormErrors from 'Services/FormErrors.ts';
+    import ManagementTab from 'Mixins/ManagementTab.ts';
+
     export default {
+        mixins : [ ManagementTab('agents') ],
         data() {
             return {
-                msg : 'Agents'
+                hasDifferentBillingDetails: false,
+                businessTerms: [
+                    { name: 'The agent recieves the full amount of the booking, then you invoice them' },
+                    { name: 'The agent only takes the deposit amount, then the rest is paid to you' },
+                    { name: 'The agent is banned from making any bookings for you' }
+                ],
+                form : new Form({
+                    resource : 'agent',
+                    action   : 'Create',
+                    errors   : new FormErrors(),
+                    data     : {
+                        id             : null,
+                        name           : '',
+                        website        : '',
+                        branch_name    : '',
+                        branch_address : '',
+                        branch_phone   : '',
+                        branch_email   : '',
+                        billing_address: '',
+                        commission     : 0
+                    }
+                })
             }
         }
     }
