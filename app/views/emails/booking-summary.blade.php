e73bb15f5af34ac0105c4e8f6c0bf259e0cb2305
Bryan Kneis <bryan@iqwebcreations.com>
2017-02-24 14:05:56 +0000

    Update booking confirmation email template to be more responsive

--- app/views/emails/booking-summary.blade.php
+++ app/views/emails/booking-summary.blade.php
@@ -56,28 +56,14 @@
 								<table cellspacing="0" cellpadding="0" width="100%" style="border-collapse:separate !important;">
 									<tr>
 										<td class="mini-block">
-											<span class="header-sm">Lead Customer</span><br />
-											{{{$booking->lead_customer->firstname}}} {{{$booking->lead_customer->lastname}}}<br />
-											@if($booking->lead_customer->address_1)
-												{{{$booking->lead_customer->address_1}}}<br />
-											@endif
-											@if($booking->lead_customer->address_2)
-												{{{$booking->lead_customer->address_2}}}<br />
-											@endif
-											@if($booking->lead_customer->city)
-												{{$booking->lead_customer->city}},
-											@endif
-											@if($booking->lead_customer->county)
-												{{$booking->lead_customer->county}},
-											@endif
-											@if($booking->lead_customer->postcode)
-												{{$booking->lead_customer->postcode}}
-											@endif
+											Lead Customer<br />
+											<strong>
+												{{{$booking->lead_customer->firstname}}} {{{$booking->lead_customer->lastname}}}
+											</strong>
 											<br />
-											@if($booking->lead_customer->country)
-												@if($booking->lead_customer->country->name)
-													{{{$booking->lead_customer->country->name}}}
-												@endif
+											@if($booking->lead_customer->phone)
+												Telephone : <br />
+												{{{$booking->lead_customer->phone}}}<br />
 											@endif
 										</td>
 									</tr>
@@ -88,10 +74,10 @@
 									<tr>
 										<td class="mini-block">
 											Booking Reference<br />
-											<span class="header-sm">{{$booking->reference}}</span><br />
+											<strong>{{$booking->reference}}</strong><br />
 											<br />
 											Booking Date<br />
-											<span class="header-sm">{{friendlyDateNoTime($booking->created_at_local)}}</span>
+											<strong>{{friendlyDateNoTime($booking->created_at_local)}}</strong>
 										</td>
 									</tr>
 								</table>
@@ -111,19 +97,17 @@
 
 									@if(!isObjectEmpty($booking->bookingdetails))
 										<tr>
-											<td class="title-dark">
+											<td style="padding-bottom: 10px !important;" class="title-dark">
 												 Trips &amp; Classes
 											</td>
-											<td class="title-dark" width="100"></td>
-											<td class="title-dark" width="100"></td>
 										</tr>
 
 										@foreach($booking->bookingdetails as $detail)
 											<tr>
-												<td class="item-col" colspan="3">
+												<td class="item-col" style="padding-bottom: 20px;">
 													<table cellspacing="0" cellpadding="0" width="100%">
 														<tr>
-															<td class="item-col-inner title" colspan="2">
+															<td class="item-col-inner title">
 																<span style="color: #4d4d4d; font-weight:bold; font-size: 17px;">
 																	@if(!empty($detail->ticket))
 																		<i class="fa fa-ship fa-fw"></i>
@@ -142,7 +126,7 @@
 																	@endif
 																</span>
 
-																<span style="color: #4d4d4d; font-size: 14px; display: block; margin-top: 5px; margin-left: 28px; margin-bottom: -15px;">
+																<span style="color: #4d4d4d; font-size: 14px; display: block; margin-top: 5px; margin-bottom: -15px; padding-bottom: 10px !important;">
 																	@if($detail->temporary)
 																		No date set
 																	@else
@@ -161,7 +145,7 @@
 															<td class="item-col-inner item" style="padding-left: 28px;">
 																<table cellspacing="0" cellpadding="0" width="100%">
 																	<tr>
-																		<td style="width: 90px;">
+																		<td style="width: 90px; padding-bottom: 10px !important;">
 																			<span style="color: #4d4d4d; font-weight:bold;">Customer:</span>
 																		</td>
 																		<td>
@@ -171,7 +155,7 @@
 
 																	@if(!empty($detail->ticket))
 																		<tr>
-																			<td>
+																			<td style="padding-bottom: 10px !important;">
 																				<span style="color: #4d4d4d; font-weight:bold;">Ticket:</span>
 																			</td>
 																			<td>
@@ -182,7 +166,7 @@
 
 																	@if(!empty($detail->training))
 																		<tr>
-																			<td>
+																			<td style="padding-bottom: 10px !important;">
 																				<span style="color: #4d4d4d; font-weight:bold;">Class:</span>
 																			</td>
 																			<td>
@@ -190,18 +174,14 @@
 																			</td>
 																		</tr>
 																	@endif
-																</table>
-															</td>
-															<td class="item-col-inner item">
-																<table cellspacing="0" cellpadding="0" width="100%">
-																	@if(!empty($detail->addons))
+																	@if(!$detail->addons->isEmpty())
 																		<tr>
-																			<td style="width: 90px;">
+																			<td style="width: 90px; padding-bottom: 10px !important;">
 																				<span style="color: #4d4d4d; font-weight:bold;">Addons:</span>
 																			</td>
 																			<td>
 																				@foreach($detail->addons as $addon)
-																					{{{$addon->name}}} <small><span class="badge badge-default">{{$addon->pivot->quantity}}</span></small><br />
+																					{{{$addon->name}}} <small><span>x {{$addon->pivot->quantity}}</span></small><br />
 																				@endforeach
 																			</td>
 																		</tr>
@@ -209,7 +189,7 @@
 
 																	@if(!empty($detail->course))
 																		<tr>
-																			<td style="padding-bottom: 0; width: 90px;">
+																			<td style="padding-bottom: 10px !important; width: 90px;">
 																				<span style="color: #4d4d4d; font-weight:bold;">Course:</span>
 																			</td>
 																			<td style="padding-bottom: 0;">
@@ -220,7 +200,7 @@
 
 																	@if(!empty($detail->packagefacade))
 																		<tr>
-																			<td style="padding-top: 0; width: 90px;">
+																			<td style="padding-top: 10px !important; width: 90px;">
 																				<span style="color: #4d4d4d; font-weight:bold;">Package:</span>
 																			</td>
 																			<td style="padding-top: 0;">
@@ -291,236 +271,5 @@
 				</center>
 			</td>
 		</tr>
-	<?php echo("<table>"); /*
-
-		<tr><td>&nbsp;</td><tr>
-
-		<tr>
-			<td align="center" valign="top" width="100%" style="background-color: #ffffff;	border-top: 1px solid #e5e5e5; border-bottom: 1px solid #e5e5e5;">
-				<center>
-					<table cellpadding="0" cellspacing="0" width="600" class="w320">
-						<tr>
-							<td class="item-table">
-								<table cellspacing="0" cellpadding="0" width="100%">
-									<tr>
-										<td class="title-dark">
-											 Price Breakdown
-										</td>
-										<td class="title-dark" width="100"></td>
-										<td class="title-dark" width="100"></td>
-									</tr>
-									{{#each packagesSummary}}
-										<tr>
-											<td class="inner-item-col">
-												<i class="fa fa-tags fa-fw"></i> {{{name}}}
-											</td>
-											<td class="inner-item-col" style="text-align: right;">
-												x1
-											</td>
-											<td class="inner-item-col" style="text-align: right; padding-right: 20px;">
-												{{currency}} {{decimal_price}}
-											</td>
-										</tr>
-									{{/each}}
-									{{#each coursesSummary}}
-										<tr>
-											<td class="inner-item-col">
-												<i class="fa fa-graduation-cap fa-fw"></i> {{{name}}}
-											</td>
-											<td class="inner-item-col" style="text-align: right;">
-												x1
-											</td>
-											<td class="inner-item-col" style="text-align: right; padding-right: 20px;">
-												{{currency}} {{decimal_price}}
-											</td>
-										</tr>
-									{{/each}}
-									{{#each ticketsSummary}}
-										<tr>
-											<td class="inner-item-col">
-												<i class="fa fa-ticket fa-fw"></i> {{{name}}}
-											</td>
-											<td class="inner-item-col" style="text-align: right;">
-												x1
-											</td>
-											<td class="inner-item-col" style="text-align: right; padding-right: 20px;">
-												{{currency}} {{decimal_price}}
-											</td>
-										</tr>
-									{{/each}}
-									{{#each addonsSummary}}
-										<tr>
-											<td class="inner-item-col">
-												<i class="fa fa-cart-plus fa-fw"></i> {{{name}}}
-											</td>
-											<td class="inner-item-col" style="text-align: right;">
-												x{{qtySummary}}
-											</td>
-											<td class="inner-item-col" style="text-align: right; padding-right: 20px;">
-												{{currency}} {{decimal_price}}
-											</td>
-										</tr>
-									{{/each}}
-									{{#each accommodations}}
-										{{#unless pivot.packagefacade_id}}
-											<tr>
-												<td class="inner-item-col">
-													<i class="fa fa-bed fa-fw"></i> {{{name}}}
-												</td>
-												<td class="inner-item-col" style="text-align: right;">
-													{{numberOfNights pivot.start pivot.end}}
-												</td>
-												<td class="inner-item-col" style="text-align: right; padding-right: 20px;">
-													{{currency}} {{decimal_price}}
-												</td>
-											</tr>
-										{{/unless}}
-									{{/each}}
-
-									<tr>
-										<td class="item-col item" style="border-top: 1px solid #cccccc;">
-										</td>
-										<td class="item-col quantity" style="text-align: right; padding-right: 10px; border-top: 1px solid #cccccc;">
-											<span class="total-space">Subtotal</span><br />
-
-											{{#compare discount '!==' '0.00'}}
-												<span class="total-space">Discount</span><br />
-											{{/compare}}
-
-											{{!-- <span class="total-space">Tax</span><br /> --}}
-
-											{{#if agent_id}}
-												<span class="total-space" style="font-weight: bold; color: #4d4d4d">Gross</span><br />
-												<span class="total-space">{{commission_percentage agent_id}} Commission</span><br />
-												<span class="total-space" style="font-weight: bold; color: #4d4d4d">Net</span>
-											{{else}}
-												<span class="total-space" style="font-weight: bold; color: #4d4d4d">Total</span>
-											{{/if}}
-										</td>
-										<td class="item-col price" style="text-align: right; border-top: 1px solid #cccccc; padding-right: 20px;">
-											<span class="total-space">{{decimal_price_without_discount_applied}}</span><br />
-
-											{{#compare discount '!==' '0.00'}}
-												<span class="total-space">-{{discount}}</span><br />
-											{{/compare}}
-
-											{{!-- <span class="total-space">$0.75</span><br /> --}}
-
-											{{#if agent_id}}
-												<span class="total-space" style="font-weight: bold; color: #4d4d4d;">{{currency}} {{decimal_price}}</span><br />
-												<span class="total-space">-{{commission_amount agent_id decimal_price}}</span><br />
-												<span style="font-weight: bold; color: #4d4d4d; border-bottom: 1px solid;">{{currency}} {{commission_result agent_id decimal_price}}</span>
-											{{else}}
-												<span style="font-weight: bold; color: #4d4d4d; border-bottom: 1px solid;">{{currency}} {{decimal_price}}</span>
-											{{/if}}
-										</td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-					</table>
-				</center>
-			</td>
-		</tr>
-
-		<tr><td>&nbsp;</td><tr>
-
-		<tr>
-			<td align="center" valign="top" width="100%" style="background-color: #ffffff;	border-top: 1px solid #e5e5e5; border-bottom: 1px solid #e5e5e5;">
-				<center>
-					<table cellpadding="0" cellspacing="0" width="600" class="w320">
-						<tr>
-							<td class="item-table">
-								<table cellspacing="0" cellpadding="0" width="100%">
-									<tr>
-										<td class="title-dark">
-											 Booking Status
-										</td>
-										<td class="title-dark" width="100"></td>
-										<td class="title-dark" width="100"></td>
-									</tr>
-									<tr>
-										<td class="inner-item-col">
-											<h4 id="status">{{statusIcon}}</h4>
-											<p>{{sourceIcon}}<p>
-										</td>
-										<td></td>
-										<td></td>
-									</tr>
-								</table>
-							</td>
-						</tr>
-					</table>
-				</center>
-			</td>
-		</tr>
-
-		{{#compare status '!==' 'temporary'}}
-			<tr>
-				<td align="center" valign="top" width="100%">
-					<center>
-						<table cellpadding="0" cellspacing="0" width="600" class="w320">
-							<tr>
-								<td class="item-table">
-									<table cellspacing="0" cellpadding="0" width="100%">
-										<tr>
-											<td class="title-dark" width="50%">
-												 Options
-											</td>
-											<td class="title-dark" width="50%"></td>
-										</tr>
-										<tr>
-											<td style="vertical-align: middle; border-right: 1px solid #ccc;">
-												<button class="btn btn-success btn-block save-booking mb10"{{saveable}}><i class="fa fa-save fa-fw"></i> Save For Later</button>
-												{{#unless price}}
-													<button class="btn btn-primary btn-block confirm-booking mb10"><i class="fa fa-check fa-fw"></i> Confirm booking</button>
-												{{else}}
-													{{#if agent_id}}
-														<button class="btn btn-primary btn-block confirm-booking mb10"><i class="fa fa-check fa-fw"></i> Confirm booking</button>
-													{{else}}
-														<button onclick="addTransaction();" class="btn btn-primary btn-block add-transaction"><i class="fa fa-credit-card fa-fw"></i> Add Transaction</button>
-													{{/if}}
-												{{/unless}}
-											</td>
-											<td>
-												<h4 class="text-center">Reserve Booking</h4>
-												<form id="reserve-booking" class="form-horizontal">
-													<div class="form-group">
-														<div class="radio col-md-12">
-															<label>
-																<input type="radio" name="email" id="email-yes" value="1" checked>
-																Send confirmation email to customer
-															</label>
-														</div>
-														<div class="radio col-md-12">
-															<label>
-																<input type="radio" name="email" id="email-no" value="0">
-																Do not send email
-															</label>
-														</div>
-													</div>
-													<div class="form-group">
-														<label for="reserve-until" class="col-sm-6 control-label">Reserve for (hours)</label>
-														<div class="col-md-6">
-															<input id="reserve-until" name="reserved_until" class="form-control" type="number" value="24">
-														</div>
-													</div>
-													<div class="form-group">
-														<div class="col-md-12">
-															<button class="btn btn-warning btn-block"><i class="fa fa-clock-o fa-fw"></i> Reserve</button>
-														</div>
-													</div>
-												</form>
-											</td>
-										</tr>
-									</table>
-								</td>
-							</tr>
-						</table>
-					</center>
-				</td>
-			</tr>
-		{{/compare}}
-	</table>
-	*/ ?>
+	<?php echo("<table>"); ?>
 @stop
