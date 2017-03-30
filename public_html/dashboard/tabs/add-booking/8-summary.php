5b14ff523208597b430e8f41e11eb801c265049f
Bryan Kneis <bryan@iqwebcreations.com>
2017-03-30 11:23:26 +0100

    Capatilize labels

--- public_html/dashboard/tabs/add-booking/8-summary.php
+++ public_html/dashboard/tabs/add-booking/8-summary.php
@@ -376,7 +376,7 @@
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button id="btn-override-price" class="btn btn-primary">Apply price change</button>
+                <button id="btn-override-price" class="btn btn-primary">Apply Price Change</button>
             </div>
 		</div>
 	</div>
